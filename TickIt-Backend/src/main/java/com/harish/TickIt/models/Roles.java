b245dd01c6d2033b2568fb3b507e4787aa50381a
Harish Sivakumar <harishss.2k07@gmail.com>
2026-03-26 16:05:59 +0530

    roles entity define

---
+++ TickIt-Backend/src/main/java/com/harish/TickIt/models/Roles.java
@@ -0,0 +1,34 @@
+package com.harish.TickIt.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Roles
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String roleName;
+	
+	public Roles(String r)
+	{
+		this.roleName = r;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getRoleName() {
+		return roleName;
+	}
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+}
