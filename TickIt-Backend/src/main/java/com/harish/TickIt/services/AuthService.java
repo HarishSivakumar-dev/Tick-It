cf7651e2ff711d75b8ade92f7f682158c84e56b5
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-02 10:10:00 +0530

    Add Role Repository integration in AuthService and create .gitattributes file

--- TickIt-Backend/src/main/java/com/harish/TickIt/services/AuthService.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/services/AuthService.java
@@ -10,6 +10,7 @@ import com.harish.TickIt.dtos.UserLoginDto;
 import com.harish.TickIt.dtos.UserRegDto;
 import com.harish.TickIt.models.Roles;
 import com.harish.TickIt.models.UserRegistration;
+import com.harish.TickIt.repositories.RoleRepo;
 import com.harish.TickIt.repositories.UserRegRepo;
 import com.harish.TickIt.util.JwtUtil;
 
@@ -18,11 +19,14 @@ public class AuthService
 {
 	@Autowired
 	private UserRegRepo rep;
+	@Autowired
+	private RoleRepo rolerep;
 	
 	BCryptPasswordEncoder bpe= new BCryptPasswordEncoder(12);
 	
 	@Autowired
 	private AuthenticationManager authenticationManager;
+	
 	@Autowired
 	private JwtUtil jwtUtil;
 	
@@ -33,7 +37,10 @@ public class AuthService
 			throw new UserAlreadyRegisteredException("user already in the db");
 		}
 		
-		Roles rl= new Roles("ROLE_USER");
+		Roles rl= rolerep.findByRoleName("ROLE_USER").orElseGet(() -> {
+			Roles newRole = new Roles("ROLE_USER");
+			return rolerep.save(newRole);
+		});
 	
 		UserRegistration ur= new UserRegistration();
 		ur.setEmail(dto.getEmail());
