36546bd100ced2a21f6f484e1776eee37793aa37
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-08 15:37:07 +0530

    Add ResponseWrapperImpl to wrap UserProfile data into ProfileResponseDto

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/wrapperimpl/ResponseWrapperImpl.java
@@ -0,0 +1,38 @@
+package com.harish.TickIt.UserService.wrapperimpl;
+
+import com.harish.TickIt.UserService.dtos.ProfileResponseDto;
+import com.harish.TickIt.UserService.models.UserProfile;
+
+public class ResponseWrapperImpl implements com.harish.TickIt.UserService.wrappers.ProfileWrapper
+{
+
+	@Override
+	public ProfileResponseDto getProfileResponseWrapper(UserProfile userProfile)
+	{
+		ProfileResponseDto responseDto=new ProfileResponseDto();
+		
+		responseDto.setName(userProfile.getName());
+		responseDto.setEmail(userProfile.getEmail());
+		responseDto.setEmployeeId(userProfile.getEmployeeId());
+		responseDto.setDepartment(userProfile.getDepartment());
+		responseDto.setContactNumber(userProfile.getContactNumber());
+		responseDto.setProfilePictureUrl(userProfile.getProfilePictureUrl());
+		responseDto.setAddress(userProfile.getAddress());
+		responseDto.setDateOfBirth(userProfile.getDateOfBirth());
+		responseDto.setGender(userProfile.getGender());
+		responseDto.setAccountCreationDate(userProfile.getAccountCreationDate());
+		responseDto.setNoOfTicketsAssigned(userProfile.getNoOfTicketsAssigned());
+		responseDto.setNoOfTicketsResolved(userProfile.getNoOfTicketsResolved());
+		responseDto.setNoOfTicketsActionPending(userProfile.getNoOfTicketsActionPending());
+		responseDto.setLastLoginTime(userProfile.getLastLoginTime());
+		responseDto.setAccountCreationDate(userProfile.getAccountCreationDate());
+		responseDto.setAccountStatus(userProfile.getAccountStatus());
+		responseDto.setNoOfTicketsExcalated(userProfile.getNoOfTicketsExcalated());
+		responseDto.setStrikeCount(userProfile.getStrikeCount());
+		responseDto.setLastStrikeDate(userProfile.getLastStrikeDate());
+		responseDto.setPerformanceRating(userProfile.getPerformanceRating());
+		
+		return responseDto;
+	}
+
+}
