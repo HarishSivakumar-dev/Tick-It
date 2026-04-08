66a35008f4850a852aa389b75c3d8eda759847de
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-08 15:30:25 +0530

    Add JwtAuthenticationFilter to handle JWT token validation and authentication

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/authentication/JwtAuthenticationFilter.java
@@ -0,0 +1,64 @@
+package com.harish.TickIt.UserService.authentication;
+
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtAuthenticationFilter extends org.springframework.web.filter.OncePerRequestFilter
+{
+	
+	@Autowired
+	private com.harish.TickIt.UserService.util.JwtUtil jwtUtil;
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException
+	{
+		String path = request.getRequestURI();
+		if(path.equals("/api/register") || path.equals("/api/login"))
+		{
+			filterChain.doFilter(request, response);
+			return;
+		}
+		
+		String authHeader = request.getHeader("Authorization");
+		
+		if(authHeader != null && authHeader.startsWith("Bearer "))
+		{
+			String token = authHeader.substring(7);
+			try
+			{
+				if(jwtUtil.validateToken(token))
+				{
+					String username = jwtUtil.getUsernameFromToken(token);
+					java.util.Set<org.springframework.security.core.authority.SimpleGrantedAuthority> auh= jwtUtil.getRolesFromToken(token).stream()
+																						  .map(r-> new org.springframework.security.core.authority.SimpleGrantedAuthority(r))
+																						  .collect(java.util.stream.Collectors.toSet());
+					
+					org.springframework.security.core.Authentication auth = new org.springframework.security.authentication.UsernamePasswordAuthenticationToken(username, null, auh);
+					org.springframework.security.core.context.SecurityContextHolder.getContext().setAuthentication(auth);
+					filterChain.doFilter(request, response);
+				}
+				else
+				{
+					throw new org.springframework.security.authentication.BadCredentialsException("Invalid JWT token");
+				}
+			}
+			catch(Exception e)
+			{
+				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				response.getWriter().write("Unauthorized: " + e.getMessage());
+			}
+			
+		}
+		else
+		{
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			response.getWriter().write("Unauthorized: Missing or invalid Authorization header");
+		}
+	}
+
+}
