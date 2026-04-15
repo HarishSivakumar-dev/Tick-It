1d4430ae1d2fb509e1504b213725d5e9f77b0629
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-15 10:42:36 +0530

    Remove empty constructor from UserProfile model

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/models/UserProfile.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/models/UserProfile.java
@@ -2,7 +2,6 @@ package com.harish.TickIt.UserService.models;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
