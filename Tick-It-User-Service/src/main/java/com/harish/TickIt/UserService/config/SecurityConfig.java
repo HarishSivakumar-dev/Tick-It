62b66611bd40946781b141c1932628551cc03cc0
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-15 10:44:26 +0530

    Add SecurityConfig to configure security settings and JWT authentication

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.harish.TickIt.UserService.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import com.harish.TickIt.UserService.authentication.JwtAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig
+{
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity http) throws Exception
+	{
+		return http.csrf(r->r.disable())
+				   .formLogin(f -> f.disable())
+				   .httpBasic(b -> b.disable())
+				   .authorizeHttpRequests(r->r.requestMatchers("/api/v1/user-profile/createProfile").permitAll().anyRequest().authenticated())
+				   .sessionManagement(s->s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				   .addFilterBefore(new JwtAuthenticationFilter(), org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class)
+				   .build();
+			
+	}
+	
+	@Bean
+	public org.springframework.security.authentication.AuthenticationManager authenticationManager(HttpSecurity http) throws Exception
+	{
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+				   .build();
+	}
+
+}
