ffe3613474f0ec1326185312fa9b899ace5f18ce
Harish Sivakumar <harishss.2k07@gmail.com>
2026-04-15 10:42:26 +0530

    Add UserProfileController with endpoints for profile retrieval and creation

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserProfileController.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserProfileController.java
@@ -3,9 +3,13 @@ package com.harish.TickIt.UserService.controllers;
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.harish.TickIt.UserService.dtos.ProfileResponseDto;
+import com.harish.TickIt.UserService.dtos.UserProfileDto;
 import com.harish.TickIt.UserService.services.UserProfileService;
 
 @RestController
@@ -16,11 +20,20 @@ public class UserProfileController
 	@Autowired
 	private UserProfileService userProfileService;
 	
+	@GetMapping("/getProfile")
 	public ResponseEntity<ProfileResponseDto> getUserProfile()
 	{
 		ProfileResponseDto userProfile=userProfileService.userProfileRetriever();
 		
 		return ResponseEntity.status(HttpStatus.SC_OK).body(userProfile);
+	}
+	
+	@PostMapping("/createProfile")
+	public ResponseEntity<String> createUserProfile(@RequestBody UserProfileDto dto)
+	{
+		String res= userProfileService.createUserProfile(dto);
+		
+		return ResponseEntity.status(HttpStatus.SC_OK).body(res);
 	}
 	
 	
