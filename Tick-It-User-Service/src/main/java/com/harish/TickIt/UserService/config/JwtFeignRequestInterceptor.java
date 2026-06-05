f0814094879660a74a07759b2867cc06179f60d9
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-05 15:04:37 +0530

    Add JwtFeignRequestInterceptor for JWT handling in Feign requests

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/JwtFeignRequestInterceptor.java
@@ -0,0 +1,14 @@
+package com.harish.TickIt.UserService.config;
+
+import feign.RequestTemplate;
+
+public class JwtFeignRequestInterceptor implements feign.RequestInterceptor
+{
+
+	@Override
+	public void apply(RequestTemplate template) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
