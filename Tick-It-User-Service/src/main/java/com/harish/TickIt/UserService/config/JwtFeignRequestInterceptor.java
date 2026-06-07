387da963afc6e3feccc537e921e5ccef9cb016b0
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-07 13:56:27 +0530

    Enhance JwtFeignRequestInterceptor to include Authorization header from the request

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/JwtFeignRequestInterceptor.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/JwtFeignRequestInterceptor.java
@@ -1,13 +1,24 @@
 package com.harish.TickIt.UserService.config;
 
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import feign.RequestTemplate;
 
 public class JwtFeignRequestInterceptor implements feign.RequestInterceptor
 {
 
 	@Override
-	public void apply(RequestTemplate template) {
+	public void apply(RequestTemplate template)
+	{
 		// TODO Auto-generated method stub
+		RequestAttributes ra =RequestContextHolder.getRequestAttributes();
+		
+		ServletRequestAttributes sra = (ServletRequestAttributes) ra;
+		
+		String token=sra.getRequest().getHeader("Authorization");
+		
+		template.header("Authorization", token);
 		
 	}
 
