83bfd076358d076627529645aa1574bd965659c1
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-08 22:52:20 +0530

    Add TicketFeignClient integration to UserActionController for ticket creation and retrieval

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserActionController.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserActionController.java
@@ -1,17 +1,41 @@
 package com.harish.TickIt.UserService.controllers;
 
+import java.util.List;
+
 import org.apache.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.harish.TickIt.UserService.dtos.TicketResponseDto;
+import com.harish.TickIt.UserService.feign.TicketFeignClient;
 
 @RestController
 @RequestMapping("/api/v1/user-action")
 public class UserActionController
 {
-	public ResponseEntity<String> createTicket()
+	@Autowired
+	private TicketFeignClient ticketFeignClient;
+	
+	
+	@PostMapping("/create-ticket")
+	public ResponseEntity<String> createTicket(@RequestBody com.harish.TickIt.UserService.dtos.TicketDetailsDto dto)
 	{
-		return ResponseEntity.status(HttpStatus.SC_OK).body("Ticket Created");
+		ResponseEntity<String> st=ticketFeignClient.ticketCreation(dto);
+		
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st.getBody());
+	}
+	
+	@GetMapping("/project-tickets/{projectId}")
+	public ResponseEntity<List<com.harish.TickIt.UserService.dtos.TicketResponseDto>> getProjectTickets(@PathVariable int projectId)
+	{	
+		ResponseEntity<List<TicketResponseDto>> st=ticketFeignClient.getProjectTickets(projectId);
+		
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st.getBody());
 	}
 	
 
