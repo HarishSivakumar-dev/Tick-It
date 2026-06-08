bc71ca6ea82f843cc8f8cec0325fac8becd05f59
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-08 22:52:10 +0530

    Add TicketResponseDto class for ticket response representation

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/dtos/TicketResponseDto.java
@@ -0,0 +1,94 @@
+package com.harish.TickIt.UserService.dtos;
+
+import java.time.LocalDateTime;
+
+public class TicketResponseDto
+{
+	private int id;
+	private String title;
+	private String description;
+	private String status;
+	private String createdBy;
+	private String creatorMail;
+	private String creatorProfilePictureUrl;
+	private String priority;
+	private long creatorId;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	private LocalDateTime closedAt;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getCreatorMail() {
+		return creatorMail;
+	}
+	public void setCreatorMail(String creatorMail) {
+		this.creatorMail = creatorMail;
+	}
+	public String getCreatorProfilePictureUrl() {
+		return creatorProfilePictureUrl;
+	}
+	public void setCreatorProfilePictureUrl(String creatorProfilePictureUrl) {
+		this.creatorProfilePictureUrl = creatorProfilePictureUrl;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+	public long getCreatorId() {
+		return creatorId;
+	}
+	public void setCreatorId(long creatorId) {
+		this.creatorId = creatorId;
+	}
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public LocalDateTime getClosedAt() {
+		return closedAt;
+	}
+	public void setClosedAt(LocalDateTime closedAt) {
+		this.closedAt = closedAt;
+	}
+
+}
