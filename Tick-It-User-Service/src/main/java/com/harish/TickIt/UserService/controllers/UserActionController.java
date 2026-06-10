4fb5d9955da29d93b7ddaa29d03aad16d76606e9
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-10 23:50:54 +0530

    Refactor UserActionController to use UserService for ticket operations

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserActionController.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/controllers/UserActionController.java
@@ -1,7 +1,6 @@
 package com.harish.TickIt.UserService.controllers;
 
 import java.util.List;
-
 import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,38 +12,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.harish.TickIt.UserService.dtos.TicketResponseDto;
-import com.harish.TickIt.UserService.feign.TicketFeignClient;
+import com.harish.TickIt.UserService.dtos.TicketUpdateDto;
+import com.harish.TickIt.UserService.services.UserService;
 
 @RestController
 @RequestMapping("/api/v1/user-action")
 public class UserActionController
-{
+{	
 	@Autowired
-	private TicketFeignClient ticketFeignClient;
-	
+	private UserService userService;
 	
 	@PostMapping("/create-ticket")
 	public ResponseEntity<String> createTicket(@RequestBody com.harish.TickIt.UserService.dtos.TicketDetailsDto dto)
 	{
-		ResponseEntity<String> st=ticketFeignClient.ticketCreation(dto);
-		
-		return ResponseEntity.status(HttpStatus.SC_OK).body(st.getBody());
+		String st=userService.createTicket(dto);
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st);
 	}
 	
 	@GetMapping("/project-tickets/{projectId}")
 	public ResponseEntity<List<com.harish.TickIt.UserService.dtos.TicketResponseDto>> getProjectTickets(@PathVariable int projectId)
 	{	
-		ResponseEntity<List<TicketResponseDto>> st=ticketFeignClient.getProjectTickets(projectId);
-		
-		return ResponseEntity.status(HttpStatus.SC_OK).body(st.getBody());
+		List<TicketResponseDto> st=userService.getProjectTickets(projectId);
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st);
 	}
 	
 	@DeleteMapping("/delete-ticket/{ticketId}")
 	public ResponseEntity<String> deleteTicket(@PathVariable int ticketId)
 	{
-		ResponseEntity<String> st=ticketFeignClient.deleteTicket(ticketId);
-		
-		return ResponseEntity.status(HttpStatus.SC_OK).body(st.getBody());
+		String st=userService.deleteTicket(ticketId);
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st);
+	}
+	
+	@PostMapping("/update-ticket")
+	public ResponseEntity<String> updateTicket(@RequestBody TicketUpdateDto dto)
+	{
+		String st=userService.updateTicket(dto);
+		return ResponseEntity.status(HttpStatus.SC_OK).body(st);
 	}
 
 }
