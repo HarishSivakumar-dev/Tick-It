8b4e8eaa91f51fa439f7176282e54a08887a7121
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-11 22:38:00 +0530

    Add TicketRepo interface for ticket data access and retrieval

---
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketRepo.java
@@ -0,0 +1,9 @@
+package com.harish.TickIt.TicketService.repos;
+
+import java.util.List;
+
+public interface TicketRepo extends org.springframework.data.jpa.repository.JpaRepository<com.harish.TickIt.TicketService.model.Ticket, Integer>
+{
+	List<com.harish.TickIt.TicketService.model.Ticket> findByProjectIdAndStatusNotAndAssignedToIsNull(int projectId, com.harish.TickIt.TicketService.enums.TicketStatus status);
+
+}
