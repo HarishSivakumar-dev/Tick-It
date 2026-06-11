114330041035b400cd57083f9afcc7cbdbd0929e
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-11 22:34:58 +0530

    Add UserDetailsDto class for user details representation

---
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/dtos/UserDetailsDto.java
@@ -0,0 +1,43 @@
+package com.harish.TickIt.TicketService.dtos;
+
+public class UserDetailsDto
+{
+	private String name;
+	private String email;
+	private Long employeeId;
+	private String department;
+	private String profilePictureUrl;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getProfilePictureUrl() {
+		return profilePictureUrl;
+	}
+	public void setProfilePictureUrl(String profilePictureUrl) {
+		this.profilePictureUrl = profilePictureUrl;
+	}
+
+}
