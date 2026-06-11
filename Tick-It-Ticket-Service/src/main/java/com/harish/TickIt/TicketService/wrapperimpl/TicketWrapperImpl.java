7957bca465c38631730c9469a8ec000ce07e1a7f
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-11 23:06:01 +0530

    Add TicketWrapperImpl to handle ticket creation and DTO conversion

---
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/wrapperimpl/TicketWrapperImpl.java
@@ -0,0 +1,63 @@
+package com.harish.TickIt.TicketService.wrapperimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.harish.TickIt.TicketService.dtos.TicketDetailsDto;
+import com.harish.TickIt.TicketService.dtos.TicketResponseDto;
+import com.harish.TickIt.TicketService.dtos.UserDetailsDto;
+import com.harish.TickIt.TicketService.enums.TicketStatus;
+import com.harish.TickIt.TicketService.feign.UserProfileFeignClient;
+import com.harish.TickIt.TicketService.model.Ticket;
+
+public class TicketWrapperImpl implements com.harish.TickIt.TicketService.wrapper.TicketWrappers
+{
+	
+	@Autowired
+	private UserProfileFeignClient userProfileFeignClient;
+
+
+	@Override
+	public Ticket createTicket(TicketDetailsDto dto)
+	{
+		Ticket ticket = new Ticket();
+		ticket.setTitle(dto.getTitle());
+		ticket.setDescription(dto.getDescription());
+		ticket.setStatus(TicketStatus.OPEN);
+		ticket.setAssignedTo(null);
+		ticket.setCreatedAt(java.time.LocalDateTime.now());
+		ticket.setUpdatedAt(null);
+		ticket.setClosedAt(null);
+		ticket.setPriority(dto.getPriority());
+		ticket.setProjectId(dto.getProjectId());
+		
+		UserDetailsDto userDetails = userProfileFeignClient.getUserProfile().getBody();
+		
+		ticket.setCreatedBy(userDetails.getName());
+		ticket.setCreatorId(userDetails.getEmployeeId());
+		ticket.setCreatorMail(userDetails.getEmail());
+		ticket.setCreatorProfilePictureUrl(userDetails.getProfilePictureUrl());
+		
+		return ticket;
+	}
+
+
+	@Override
+	public TicketResponseDto toDto(Ticket ticket)
+	{
+			TicketResponseDto responseDto = new TicketResponseDto();
+			responseDto.setId(ticket.getId());
+			responseDto.setTitle(ticket.getTitle());
+			responseDto.setDescription(ticket.getDescription());
+			responseDto.setStatus(ticket.getStatus().toString());
+			responseDto.setPriority(ticket.getPriority().toString());
+			responseDto.setCreatedAt(ticket.getCreatedAt());
+			responseDto.setUpdatedAt(ticket.getUpdatedAt());
+			responseDto.setClosedAt(ticket.getClosedAt());
+			responseDto.setCreatedBy(ticket.getCreatedBy());
+			responseDto.setCreatorId(ticket.getCreatorId());
+			responseDto.setCreatorMail(ticket.getCreatorMail());
+			responseDto.setCreatorProfilePictureUrl(ticket.getCreatorProfilePictureUrl());
+			
+			return responseDto;
+	}
+
+}
