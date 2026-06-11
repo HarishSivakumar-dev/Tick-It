9020a2e66df08fa973aff81f28a6562dbf742daa
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-11 22:35:35 +0530

    Add main application class for TickIt Ticket Service

---
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/TickItTicketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.harish.TickIt.TicketService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickItTicketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickItTicketServiceApplication.class, args);
+	}
+
+}
