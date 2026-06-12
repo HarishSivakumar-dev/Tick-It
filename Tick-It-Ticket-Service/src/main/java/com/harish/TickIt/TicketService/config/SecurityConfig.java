a8d88c4293194892a51684b06a0a3a27615d4419
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-12 17:36:02 +0530

    Add JwtAuthenticationFilter to SecurityConfig for JWT authentication

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/SecurityConfig.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.harish.TickIt.TicketService.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.harish.TickIt.TicketService.auth.JwtAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig 
 {
+	@Autowired
+	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http)
 	{
 		return http.csrf(r->r.disable())
 					.authorizeHttpRequests(r->r.requestMatchers("/**").permitAll())
 					.httpBasic(t->t.disable())
+					.formLogin(t->t.disable())
+					.addFilter(jwtAuthenticationFilter, new UsernamePasswordAuthenticationFilter())
 					.build();
 	}
 
