5bee580b824d1a3fac05132b5d91a41ff7145255
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-13 14:23:20 +0530

    Update SecurityConfig to add JwtAuthenticationFilter before UsernamePasswordAuthenticationFilter

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/SecurityConfig.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import com.harish.TickIt.TicketService.auth.JwtAuthenticationFilter;
 
 @Configuration
@@ -22,7 +21,7 @@ public class SecurityConfig
 					.authorizeHttpRequests(r->r.requestMatchers("/**").permitAll())
 					.httpBasic(t->t.disable())
 					.formLogin(t->t.disable())
-					.addFilter(jwtAuthenticationFilter, new UsernamePasswordAuthenticationFilter())
+					.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 					.build();
 	}
 
