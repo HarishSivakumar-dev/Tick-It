f67ad39910251a82c537660030a237e8d7530bfb
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-13 19:11:44 +0530

    Implement JWT authentication logic in JwtAuthenticationFilter

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/auth/JwtAuthenticationFilter.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/auth/JwtAuthenticationFilter.java
@@ -1,8 +1,18 @@
 package com.harish.TickIt.TicketService.auth;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.harish.TickIt.TicketService.util.JwtUtil;
+
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,12 +21,42 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter
 {
+	@Autowired
+	private JwtUtil jwtUtil;
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException
 	{
 		// TODO Auto-generated method stub
+		String authHeader = request.getHeader("Authorization");
+		if(authHeader !=null && authHeader.startsWith("Bearer "))
+		{
+			String token = authHeader.substring(7);
+			
+			if(token !=null && jwtUtil.validateToken(token))
+			{
+				List<String> roles= jwtUtil.getRolesFromToken(token);
+				Set<org.springframework.security.core.authority.SimpleGrantedAuthority> authorities= roles.stream()
+																											.map(r->new org.springframework.security.core.authority.SimpleGrantedAuthority(r))
+																											.collect(java.util.stream.Collectors.toSet());
+				
+				Authentication authentication= new UsernamePasswordAuthenticationToken(jwtUtil.getUsernameFromToken(token), null, authorities);
+				SecurityContextHolder.getContext().setAuthentication(authentication);
+				filterChain.doFilter(request, response);
+				return;
+			}
+			else
+			{
+				response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid JWT Token");
+				return;
+			}
+		}
+		else
+		{
+			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing or Invalid Authorization Header");
+			return;
+		}
 		
 	}
 	
