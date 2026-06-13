63187cfa709550e1cf620f5cd6be592b49f44602
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-13 19:12:58 +0530

    Authentication object initialization

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/auth/JwtAuthenticationFilter.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/auth/JwtAuthenticationFilter.java
@@ -3,16 +3,13 @@ package com.harish.TickIt.TicketService.auth;
 import java.io.IOException;
 import java.util.List;
 import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import com.harish.TickIt.TicketService.util.JwtUtil;
-
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
