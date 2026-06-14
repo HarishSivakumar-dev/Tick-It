74ecfafe9dcb10661ede4e63ec6ec7bb851130b1
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-14 20:42:08 +0530

    Add JwtUtil class for JWT token validation and role extraction

---
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/util/JwtUtil.java
@@ -0,0 +1,39 @@
+package com.harish.TickIt.ProjectService.util;
+
+import java.util.List;
+import java.util.Set;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil
+{
+	@Value("${jwt.secret}")
+	private String SECRET_KEY;
+	
+	public boolean validateToken(String token)
+	{
+		SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token) !=null;
+	}
+	public String getUsernameFromToken(String token)
+	{
+		SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+		return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getSubject();
+	}
+	
+	public List<String> getRolesFromToken(String token)
+	{
+		SecretKey secretKey = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+		Set<?> st= Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("roles", Set.class);
+		
+		List<String> roles= st.stream()
+							   .map(r -> r.toString())
+							   .toList();
+		return roles;
+	}
+
+}
