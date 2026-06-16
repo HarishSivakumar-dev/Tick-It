39a61ef0e38f8827bb9977ad1e76e0074149d9b6
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-16 22:33:58 +0530

    add method to find ProjectDetails by project name

--- Tick-It-Project-Service/src/main/java/com/harish/TicktIt/ProjectService/repos/ProjectDetailsRepo.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TicktIt/ProjectService/repos/ProjectDetailsRepo.java
@@ -2,9 +2,10 @@ package com.harish.TicktIt.ProjectService.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.harish.TicktIt.ProjectService.models.ProjectDetails;
+import java.util.Optional;
 
 public interface ProjectDetailsRepo extends JpaRepository<ProjectDetails, Integer>
 {
-	
+	Optional<ProjectDetails> findByProjectName(String projectname);
 
 }
