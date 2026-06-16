c8981450379a3368be34d97fc88e59d7cc9aa105
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-16 22:33:39 +0530

    add project creation and update methods in ProjectService

--- Tick-It-Project-Service/src/main/java/com/harish/TicktIt/ProjectService/services/ProjectService.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TicktIt/ProjectService/services/ProjectService.java
@@ -1,8 +1,11 @@
 package com.harish.TicktIt.ProjectService.services;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import com.harish.TickIt.ProjectService.dtos.ProjectCreationDto;
 import com.harish.TickIt.ProjectService.dtos.ProjectResponseDto;
+import com.harish.TicktIt.ProjectService.models.ProjectDetails;
 import com.harish.TicktIt.ProjectService.repos.ProjectDetailsRepo;
 
 public class ProjectService
@@ -30,6 +33,46 @@ public class ProjectService
 												  })
 												  .toList();
 		return pd;
+		
+	}
+	
+	public String projectCreation(ProjectCreationDto dto)
+	{
+		ProjectDetails pd= new ProjectDetails();
+		
+		pd.setActive(dto.isActive());
+		pd.setCreatedAt(LocalDateTime.now());
+		pd.setEndDate(null);
+		pd.setProjectDescription(dto.getProjectDescription());
+		pd.setProjectManagerId(dto.getProjectManagerId());
+		pd.setProjectName(dto.getProjectName());
+		pd.setStartDate(dto.getStartDate());
+		pd.setStatus(dto.getStatus());
+		pd.setUpdatedAt(null);
+		
+		pdr.save(pd);
+		
+		return "Record Saved";
+		
+	}
+	
+	public String projectDetailsUpdation(ProjectCreationDto dto)
+	{
+		ProjectDetails det= pdr.findByProjectName(dto.getProjectName()).orElseThrow();
+		det.setUpdatedAt(LocalDateTime.now());
+		
+		if(dto.getEndDate()!=null)
+			det.setEndDate(dto.getEndDate());
+		if(dto.getProjectDescription()!=null)
+			det.setProjectDescription(dto.getProjectDescription());
+		if(dto.getStatus()!=null)
+			det.setStatus(dto.getStatus());
+		if(dto.isActive() != null)
+			det.setActive(dto.isActive());
+		
+		pdr.save(det);
+		
+		return "Updated Successfully";
 		
 	}
 }
