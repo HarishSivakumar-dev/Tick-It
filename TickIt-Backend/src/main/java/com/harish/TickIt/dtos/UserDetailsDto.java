854f0d257e399b4a89c43253d8581394dc9b7a41
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-17 23:28:59 +0530

    add UserDetailsDto for managing user information

---
+++ TickIt-Backend/src/main/java/com/harish/TickIt/dtos/UserDetailsDto.java
@@ -0,0 +1,30 @@
+package com.harish.TickIt.dtos;
+
+public class UserDetailsDto
+{
+	private Long userId;
+	private String userName;
+	private String email;
+	
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+}
