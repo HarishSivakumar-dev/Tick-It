0f4dc0605710077a47569b6497b1c9db43d76fc4
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-17 23:27:42 +0530

    add endpoint to add default user data post-registration in ProjectMemberController

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
@@ -1,22 +1,36 @@
 package com.harish.TickIt.ProjectService.controllers;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.harish.TickIt.ProjectService.dtos.UserDetailsDto;
+import com.harish.TicktIt.ProjectService.dtos.UserProjectDto;
+import com.harish.TicktIt.ProjectService.services.ProjectService;
 
 @RestController
 @RequestMapping("/app")
 public class ProjectMemberController
 {
+	@Autowired
+	private ProjectService ps;
+	
 	@GetMapping("/projects/get/members")
 	public ResponseEntity<List<UserDetailsDto>> getAllAvailableMembers()
 	{
-		//I will declare this definition with service implementation on 17/06/2026 
-		return null;
-		
+		return ResponseEntity.status(HttpStatus.OK).body(ps.getAllEmployeesUnassigned());
+	}
+	
+	@PostMapping("/projects/add/default")
+	public ResponseEntity<String> addDefaultUserDataPostRegistration(@RequestBody UserProjectDto dto)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(ps.userInfoPopulation(dto));
 	}
 
 }
