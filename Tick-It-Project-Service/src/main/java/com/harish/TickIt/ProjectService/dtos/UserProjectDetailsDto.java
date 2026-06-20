7f9c212d3b208ae27a46abe6e194400dcc28685c
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-20 22:08:04 +0530

    fix package naming in UserProjectDetailsDto and add @EnableFeignClients annotation in TickItUserServiceApplication

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/dtos/UserProjectDetailsDto.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/dtos/UserProjectDetailsDto.java
@@ -1,7 +1,8 @@
-package com.harish.TicktIt.ProjectService.dtos;
+package com.harish.TickIt.ProjectService.dtos;
 
 import java.time.LocalDate;
-import com.harish.TicktIt.ProjectService.enums.ProjectRole;
+
+import com.harish.TickIt.ProjectService.enums.ProjectRole;
 
 public class UserProjectDetailsDto 
 {
--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/TickItUserServiceApplication.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/TickItUserServiceApplication.java
@@ -2,9 +2,12 @@ package com.harish.TickIt.UserService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class TickItUserServiceApplication {
+@EnableFeignClients(basePackages = "com.harish.TickIt.UserService.feign")
+public class TickItUserServiceApplication
+{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TickItUserServiceApplication.class, args);
