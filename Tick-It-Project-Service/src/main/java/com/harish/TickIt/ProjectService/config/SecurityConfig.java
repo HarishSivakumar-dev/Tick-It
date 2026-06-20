7a21f0926bb0a1df6375ce76539c9be173dd5481
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-20 22:07:06 +0530

    fix package naming in ProjectStatus enum and add security configuration in SecurityConfig class

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/config/SecurityConfig.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/config/SecurityConfig.java
@@ -1,18 +1,24 @@
 package com.harish.TickIt.ProjectService.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.stereotype.Component;
 import com.harish.TickIt.ProjectService.auth.JwtAuthencationFilter;
 
 @Component
+@EnableWebSecurity
 public class SecurityConfig
 {
 	@Autowired
 	private JwtAuthencationFilter jwtAuthenticationFilter;
 	
+	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
 	{
 		return http.csrf(r->r.disable())
@@ -21,6 +27,13 @@ public class SecurityConfig
 				   .formLogin(r->r.disable())
 				   .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
 				   .build();
+	}
+	
+	@Bean
+	public AuthenticationManager authManager(HttpSecurity http) throws Exception
+	{
+		return http.getSharedObject(AuthenticationManagerBuilder.class)
+		           .build();
 	}
 
 }
