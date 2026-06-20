2a9e727da1dbe7b8c79ab6dd6c10188c82922138
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-20 22:07:52 +0530

    add @EnableFeignClients annotation in TickItTicketServiceApplication and @Component annotation in TicketWrappers

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/TickItTicketServiceApplication.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/TickItTicketServiceApplication.java
@@ -2,8 +2,10 @@ package com.harish.TickIt.TicketService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.harish.TickIt.TicketService.feign")
 public class TickItTicketServiceApplication {
 
 	public static void main(String[] args) {
--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/wrapper/TicketWrappers.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/wrapper/TicketWrappers.java
@@ -1,8 +1,10 @@
 package com.harish.TickIt.TicketService.wrapper;
 
+import org.springframework.stereotype.Component;
 import com.harish.TickIt.TicketService.dtos.TicketResponseDto;
 import com.harish.TickIt.TicketService.model.Ticket;
 
+@Component
 public interface TicketWrappers
 {
 	public Ticket createTicket(com.harish.TickIt.TicketService.dtos.TicketDetailsDto dto);
