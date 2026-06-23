0ff34367567c5745178aa1289f67474472fbdd6b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-23 21:39:01 +0530

    feat(UserAuthController): add endpoint to retrieve user details by user ID

--- TickIt-Backend/src/main/java/com/harish/TickIt/controllers/UserAuthController.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/controllers/UserAuthController.java
@@ -2,10 +2,14 @@ package com.harish.TickIt.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.harish.TickIt.dtos.UserFeignResponse;
 import com.harish.TickIt.dtos.UserLoginDto;
 import com.harish.TickIt.dtos.UserRegDto;
 import com.harish.TickIt.services.AuthService;
@@ -27,5 +31,11 @@ public class UserAuthController
 	public ResponseEntity<String> loginUser(@RequestBody UserLoginDto dto)
 	{
 		return ResponseEntity.status(200).body(userService.loginUser(dto));
+	}
+	
+	@GetMapping("/user/get/details/{userId}")
+	public ResponseEntity<UserFeignResponse> getUserDetails(@PathVariable Long userId) throws Exception
+	{
+		return ResponseEntity.status(200).body(userService.returnUserDetails(userId));
 	}
 }
