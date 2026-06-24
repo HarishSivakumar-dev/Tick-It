aef256a77f546453a46640dfb07326d8c1c1c26d
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-24 12:17:52 +0530

    feat(TicketRepo): add method to find tickets by project ID, status, and priority excluding assigned tickets

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketRepo.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketRepo.java
@@ -2,11 +2,15 @@ package com.harish.TickIt.TicketService.repos;
 
 import java.util.List;
 import org.springframework.stereotype.Repository;
+import com.harish.TickIt.TicketService.enums.TicketPriority;
+import com.harish.TickIt.TicketService.enums.TicketStatus;
 
 
 @Repository
 public interface TicketRepo extends org.springframework.data.jpa.repository.JpaRepository<com.harish.TickIt.TicketService.model.Ticket, Integer>
 {
 	List<com.harish.TickIt.TicketService.model.Ticket> findByProjectIdAndStatusNotAndAssignedToIsNull(int projectId, com.harish.TickIt.TicketService.enums.TicketStatus status);
+	
+	List<com.harish.TickIt.TicketService.model.Ticket> findByProjectIdAndStatusAndPriorityNotInAndAssignedToIsNull(int projectId, TicketStatus ts, List<TicketPriority> tp);
 	
 }
