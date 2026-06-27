9545d55eb7e37984105a3170d81bf8457053269f
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-27 23:53:42 +0530

    feat(UserService): remove unused ticket-related methods and dependencies

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/services/UserService.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/services/UserService.java
@@ -1,16 +1,11 @@
 package com.harish.TickIt.UserService.services;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import com.harish.TickIt.UserService.dtos.ProfileResponseDto;
-import com.harish.TickIt.UserService.dtos.TicketDetailsDto;
-import com.harish.TickIt.UserService.dtos.TicketUpdateDto;
 import com.harish.TickIt.UserService.dtos.UserProfileDto;
-import com.harish.TickIt.UserService.feign.TicketFeignClient;
 import com.harish.TickIt.UserService.models.UserProfile;
 import com.harish.TickIt.UserService.repos.UserProfileRepo;
 import com.harish.TickIt.UserService.wrapperimpl.ResponseWrapperImpl;
@@ -23,9 +18,6 @@ public class UserService
 	
 	@Autowired
 	private ResponseWrapperImpl responseWrapper;
-	
-	@Autowired
-	private TicketFeignClient ticketFeignClient;
 	
 	public ProfileResponseDto userProfileRetriever()
 	{
@@ -51,28 +43,6 @@ public class UserService
 		return "User profile created successfully";
 	}
 	
-	public String createTicket(TicketDetailsDto dto)
-	{
-		ResponseEntity<String> st=ticketFeignClient.ticketCreation(dto);
-		return st.getBody();
-	}
-	
-	public String deleteTicket(int ticketId)
-	{
-		ResponseEntity<String> st=ticketFeignClient.deleteTicket(ticketId);
-		return st.getBody();
-	}
-	
-	public String updateTicket(TicketUpdateDto dto)
-	{
-		ResponseEntity<String> st=ticketFeignClient.updateTicket(dto);
-		return st.getBody();
-	}
-	
-	public List<com.harish.TickIt.UserService.dtos.TicketResponseDto> getProjectTickets(int projectId)
-	{
-		ResponseEntity<List<com.harish.TickIt.UserService.dtos.TicketResponseDto>> st=ticketFeignClient.getProjectTickets(projectId);
-		return st.getBody();
-	}
-
 }
+
+
