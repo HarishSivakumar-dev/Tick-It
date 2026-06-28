d8ff6a23e0482928fd2e8f4e39ecc1736c8011ef
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-28 21:17:28 +0530

    feat(UserService): add methods to retrieve basic user profile and check user role

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/services/UserService.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/services/UserService.java
@@ -1,9 +1,11 @@
 package com.harish.TickIt.UserService.services;
 
 
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import com.harish.TickIt.UserService.dtos.ProfileDto;
 import com.harish.TickIt.UserService.dtos.ProfileResponseDto;
 import com.harish.TickIt.UserService.dtos.UserProfileDto;
 import com.harish.TickIt.UserService.models.UserProfile;
@@ -41,6 +43,37 @@ public class UserService
 		userProfileRepo.save(userProfile);
 		
 		return "User profile created successfully";
+	}
+	
+	public ProfileDto getBasicUserProfile()
+	{
+		ProfileResponseDto userProfile=this.userProfileRetriever();
+		
+		ProfileDto profileDto = new ProfileDto();
+		profileDto.setName(userProfile.getName());
+		profileDto.setEmail(userProfile.getEmail());
+		profileDto.setEmployeeId(userProfile.getEmployeeId());
+		profileDto.setDepartment(userProfile.getDepartment());
+		profileDto.setProfilePictureUrl(userProfile.getProfilePictureUrl());
+		
+		return profileDto;
+	}
+
+	public Boolean getUserDetails(long userId)
+	{
+		Optional<UserProfile> usr= userProfileRepo.findByEmployeeId(userId);
+		
+		if(usr.isEmpty())
+		{
+			return false;
+		}
+		else
+		{
+			if(usr.get().getRole().equals("ROLE_PROJECT_MANAGER"))
+				return true;
+			else
+			return false;
+		}	
 	}
 	
 }
