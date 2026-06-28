27c63cf4fb8eed89c4a20d6a01f52093e91d8cc4
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-28 21:51:42 +0530

    refactor(AuthService): remove unused import for Nullable annotation

--- TickIt-Backend/src/main/java/com/harish/TickIt/services/AuthService.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/services/AuthService.java
@@ -1,8 +1,6 @@
 package com.harish.TickIt.services;
 
 import java.time.LocalDateTime;
-
-import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
