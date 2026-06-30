a2f3885c1f9d6d8f5035ded1048b045eae91be2c
Harish Sivakumar <harishss.2k07@gmail.com>
2026-06-30 22:41:28 +0530

    feat(TicketRepo): add query method to filter tickets by status, priority, and approval status

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketRepo.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketRepo.java
@@ -1,7 +1,10 @@
 package com.harish.TickIt.TicketService.repos;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.harish.TickIt.TicketService.enums.TicketApprovalStatus;
 import com.harish.TickIt.TicketService.enums.TicketPriority;
 import com.harish.TickIt.TicketService.enums.TicketStatus;
 
@@ -12,5 +15,16 @@ public interface TicketRepo extends org.springframework.data.jpa.repository.JpaR
 	List<com.harish.TickIt.TicketService.model.Ticket> findByProjectIdAndStatusNotAndAssignedToIsNull(int projectId, com.harish.TickIt.TicketService.enums.TicketStatus status);
 	
 	List<com.harish.TickIt.TicketService.model.Ticket> findByProjectIdAndStatusAndPriorityNotInAndAssignedToIsNull(int projectId, TicketStatus ts, List<TicketPriority> tp);
+	
+	@Query("""
+			SELECT t from Ticket t
+			WHERE (:status IS NULL OR t.status =:status)
+			AND
+			(:status IS NULL OR t.priority =:priority)
+			AND
+			(:approval IS NULL OR t.approved =:approval)
+			
+			""")
+	List<com.harish.TickIt.TicketService.model.Ticket> filterBySpecs(@Param("status") TicketStatus status, @Param("priority") TicketPriority priority, @Param("approval") TicketApprovalStatus approval);
 	
 }
