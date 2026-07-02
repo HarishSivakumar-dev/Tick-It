3c4ed369a4ed3d14172327293cc1e7ac3b5b32c0
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-02 23:15:19 +0530

    feat(TickItProjectServiceApplication): enable Feign clients for service communication

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/TickItProjectServiceApplication.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/TickItProjectServiceApplication.java
@@ -1,9 +1,11 @@
 package com.harish.TickIt.ProjectService;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.harish.TickIt.ProjectService.feign")
 public class TickItProjectServiceApplication {
 
 	public static void main(String[] args) {
