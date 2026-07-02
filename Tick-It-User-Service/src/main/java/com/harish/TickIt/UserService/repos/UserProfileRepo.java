9b4cf403ea2b48880a728c0b0a3a5d027f446265
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-02 15:12:54 +0530

    feat(UserProfileRepo): add method to search profiles by username or email and filter by department and designation

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/repos/UserProfileRepo.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/repos/UserProfileRepo.java
@@ -1,7 +1,9 @@
 package com.harish.TickIt.UserService.repos;
 
+import java.util.List;
 import java.util.Optional;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.harish.TickIt.UserService.models.UserProfile;
 
@@ -11,4 +13,8 @@ public interface UserProfileRepo extends org.springframework.data.jpa.repository
 	Optional<UserProfile> findByEmail(String email);
 	Optional<UserProfile> findByEmployeeId(Long employeeid);
 	Optional<UserProfile> findByUserName(String username);
+	List<UserProfile> findByUserNameContainingIgnoreCaseOrEmailContainingIgnoreCase(String query, String query2);
+	
+	@Query("SELECT u FROM UserProfile u WHERE (:department IS NULL OR u.department = :department) AND (:designation IS NULL OR u.designation = :designation)")
+	List<UserProfile> findBasedOnFilter(@Param(value="department") String department, @Param(value="designation") String designation);
 }
