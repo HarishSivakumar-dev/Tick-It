7f22b4eb880128fb475c7a32a17e716ab1af66cc
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-02 16:22:29 +0530

    feat(ProjectFeignClient): change endpoint to GET for project search functionality

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/feign/ProjectFeignClient.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/feign/ProjectFeignClient.java
@@ -2,15 +2,14 @@ package com.harish.TickIt.TicketService.feign;
 
 import java.util.Optional;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.harish.TickIt.TicketService.dtos.TicketAvailDto;
 
-@FeignClient
+@FeignClient("Tick-It-Project-Service")
 public interface ProjectFeignClient
 {
-	
-	@PostMapping("/")
+	@GetMapping("/app/projects/find")
 	Optional<TicketAvailDto> getProjectIdFromService(@RequestParam long projectId);
 	
 }
