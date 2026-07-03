fd0dadfbc4011bab4afd2cf4aea82a18600766d8
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-03 15:43:19 +0530

    feat(JwtUtil): refactor secret key handling and update roles retrieval to use List

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/util/JwtUtil.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/util/JwtUtil.java
@@ -11,21 +11,22 @@ public class JwtUtil
 	@Value("${jwt.secret}")
 	private String secretKey;
 	
-	SecretKey secretKeyObj = io.jsonwebtoken.security.Keys.hmacShaKeyFor(secretKey.getBytes());
-	
 	public Boolean validateToken(String token)
 	{
+		SecretKey secretKeyObj = io.jsonwebtoken.security.Keys.hmacShaKeyFor(secretKey.getBytes());
 		return Jwts.parser().verifyWith(secretKeyObj).build().parseSignedClaims(token) !=null;
 	}
 	
 	public String getUsernameFromToken(String token)
 	{
+		SecretKey secretKeyObj = io.jsonwebtoken.security.Keys.hmacShaKeyFor(secretKey.getBytes());
 		return Jwts.parser().verifyWith(secretKeyObj).build().parseSignedClaims(token).getPayload().getSubject();
 	}
 	
 	public java.util.Set<String> getRolesFromToken(String token)
 	{
-		java.util.Set<?> role= Jwts.parser().verifyWith(secretKeyObj).build().parseSignedClaims(token).getPayload().get("roles", java.util.Set.class);
+		SecretKey secretKeyObj = io.jsonwebtoken.security.Keys.hmacShaKeyFor(secretKey.getBytes());
+		java.util.List<?> role= Jwts.parser().verifyWith(secretKeyObj).build().parseSignedClaims(token).getPayload().get("roles", java.util.List.class);
 		
 		java.util.Set<String> ro= role.stream()
 						   .map(r -> r.toString())
