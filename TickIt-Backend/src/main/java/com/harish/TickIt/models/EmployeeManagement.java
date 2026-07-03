054f57bc1b15b295f91da78666790e9609f5e9e1
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-03 23:47:26 +0530

    feat(EmployeeManagement): create EmployeeManagement entity for managing employee information

---
+++ TickIt-Backend/src/main/java/com/harish/TickIt/models/EmployeeManagement.java
@@ -0,0 +1,63 @@
+package com.harish.TickIt.models;
+
+import com.harish.TickIt.enums.Department;
+import com.harish.TickIt.enums.Designation;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "employee_information")
+public class EmployeeManagement
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	private String userName;
+	private String email;
+	private Designation designation;
+	private Department department;
+	private String password;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Designation getDesignation() {
+		return designation;
+	}
+	public void setDesignation(Designation designation) {
+		this.designation = designation;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
