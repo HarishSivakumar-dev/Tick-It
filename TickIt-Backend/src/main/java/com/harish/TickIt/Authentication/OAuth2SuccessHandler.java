b7b1eb4f73ab0063b207023823a48e516cfe7dcb
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-04 23:41:38 +0530

    feat(OAuth2SuccessHandler): simplify user registration process by utilizing AuthService for OAuth registration

--- TickIt-Backend/src/main/java/com/harish/TickIt/Authentication/OAuth2SuccessHandler.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/Authentication/OAuth2SuccessHandler.java
@@ -6,12 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
-import com.harish.TickIt.dtos.UserDetailsDto;
-import com.harish.TickIt.dtos.UserProfileDto;
-import com.harish.TickIt.feign.UserServiceFeign;
-import com.harish.TickIt.models.Roles;
-import com.harish.TickIt.repositories.RoleRepo;
+import com.harish.TickIt.models.UserRegistration;
 import com.harish.TickIt.repositories.UserRegRepo;
+import com.harish.TickIt.services.AuthService;
 import com.harish.TickIt.util.JwtUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -26,11 +23,7 @@ public class OAuth2SuccessHandler implements org.springframework.security.web.au
 	@Autowired
 	private UserRegRepo rep;
 	@Autowired
-	private RoleRepo rolerep;
-	@Autowired
-	private UserServiceFeign userServiceFeign;
-	@Autowired
-	private com.harish.TickIt.feign.ProjectServiceFeign project;
+	private AuthService ser;
 
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
@@ -51,38 +44,8 @@ public class OAuth2SuccessHandler implements org.springframework.security.web.au
 		}
 		else
 		{
-			Roles rl= rolerep.findByRoleName("ROLE_USER").orElseGet(() -> {
-				Roles newRole = new Roles("ROLE_USER");
-				return rolerep.save(newRole);
-			});
-			
-			usr= new com.harish.TickIt.models.UserRegistration();
-			usr.setEmail(email);
-			usr.setRegistrationDate(java.time.LocalDateTime.now());
-			usr.setUserName(name);
-			usr.getRoles().add(rl);
-			
-			com.harish.TickIt.models.UserRegistration newusr=rep.save(usr);
-			
-			UserProfileDto updto= new UserProfileDto();
-			updto.setEmail(newusr.getEmail());
-			updto.setUserName(newusr.getUserName());
-			updto.setEmployeeId(newusr.getId());
-			updto.setDepartment(null); // Set department to null or any default value
-			updto.setProfileCreatedAt(newusr.getRegistrationDate());
-			
-			String res=userServiceFeign.createUserProfile(updto).getBody();
-			
-			UserDetailsDto dt= new UserDetailsDto();
-			dt.setEmail(newusr.getEmail());
-			dt.setUserId(newusr.getId());
-			dt.setUserName(newusr.getUserName());
-		
-			String resp= project.saveUserDetails(dt).getBody();
-			
-			System.out.println("User profile creation response: " + res);
-			System.out.println("Project service response: " + resp);
-			token= jwtUtil.generateToken(newusr);	
+			UserRegistration reg= ser.registerUserOauth(email, name);
+			token= jwtUtil.generateToken(reg);
 		}	
 		
 		response.setHeader("Authorization", "Bearer "+token);
