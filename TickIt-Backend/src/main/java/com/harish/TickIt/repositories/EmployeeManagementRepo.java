6722eb50a520fb0f21c558d636b52fdffda012bc
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-04 23:41:18 +0530

    feat(EmployeeManagementRepo): add findByEmailAndId method to retrieve employee by email and ID

--- TickIt-Backend/src/main/java/com/harish/TickIt/repositories/EmployeeManagementRepo.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/repositories/EmployeeManagementRepo.java
@@ -1,9 +1,12 @@
 package com.harish.TickIt.repositories;
 
+import java.util.Optional;
 import org.springframework.stereotype.Repository;
+import com.harish.TickIt.models.EmployeeManagement;
 
 @Repository
 public interface EmployeeManagementRepo extends org.springframework.data.jpa.repository.JpaRepository<com.harish.TickIt.models.EmployeeManagement, Long>
 {
-	
+	Optional<com.harish.TickIt.models.EmployeeManagement> findByEmail(String email);
+	Optional<EmployeeManagement> findByEmailAndId(String email, Long id);
 }
