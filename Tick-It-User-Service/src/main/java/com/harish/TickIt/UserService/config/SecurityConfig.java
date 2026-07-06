2279c84de6145291414c0c1fac60d0167ea7e20d
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-06 09:11:07 +0530

    feat(SecurityConfig): update role hierarchy to include ROLE_LEAD

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/SecurityConfig.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig
 	@Bean
 	public org.springframework.security.access.hierarchicalroles.RoleHierarchy roleHierarchy()
 	{
-		return org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_MANAGER > ROLE_USER");
+		return org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl.fromHierarchy("ROLE_ADMIN > ROLE_MANAGER > ROLE_LEAD > ROLE_USER");
 	}
 	
 	@Bean
