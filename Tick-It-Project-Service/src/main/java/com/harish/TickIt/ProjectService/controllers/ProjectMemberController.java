06d7b6771b03e507fc31c7be09d422fef62b0239
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-06 09:10:32 +0530

    feat(ProjectMemberController): add method-level security for MANAGER and ADMIN roles

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
@@ -1,10 +1,10 @@
 package com.harish.TickIt.ProjectService.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +23,7 @@ public class ProjectMemberController
 	private ProjectService ps;
 	
 	@GetMapping("/projects/get/members")
+	@PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
 	public ResponseEntity<List<UserDetailsDto>> getAllAvailableMembers()
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(ps.getAllEmployeesUnassigned());
@@ -35,6 +36,7 @@ public class ProjectMemberController
 	}
 	
 	@GetMapping("/myProjects/get")
+	@PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
 	public ResponseEntity<List<UserProjectDetailsDto>> getProjectsForUser()
 	{
 		return ResponseEntity.status(HttpStatus.OK).body(ps.getAllUserProjects());
