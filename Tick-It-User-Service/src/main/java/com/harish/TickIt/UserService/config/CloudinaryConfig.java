604945210a00cbb0bf89ee1ce39bdee0a650d17f
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-07 11:18:25 +0530

    Addition of definition of the cloudinary bean with tickit account credentials for image storage API with cloudinary SDK

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/CloudinaryConfig.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/CloudinaryConfig.java
@@ -2,6 +2,7 @@ package com.harish.TickIt.UserService.config;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.cloudinary.Cloudinary;
@@ -9,13 +10,21 @@ import com.cloudinary.Cloudinary;
 @Configuration
 public class CloudinaryConfig
 {
+	@Value("${cloudinary.cloud-name}")
+	private String cloudName;
+	@Value("${cloudinary.api-key}")
+	private String apiKey;
+	@Value("${cloudinary.api-secret}")
+	private String apiSecret;
+	
+	
 	@Bean
 	public Cloudinary cloudinary()
 	{
 		Map<String, String> config = new HashMap<>();
-		config.put("cloud_name", "your_cloud_name");
-		config.put("api_key", "your_api_key");
-		config.put("api_secret", "your_api_secret");
+		config.put("cloud_name", cloudName);
+		config.put("api_key", apiKey);
+		config.put("api_secret", apiSecret);
 		
 		return new Cloudinary(config);
 	}
