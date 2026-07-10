003c3341c6db76bf9d3ae53661b09d742daddae7
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-10 14:19:04 +0530

    feat(security): implement JwtUtil for JWT token verification and user role extraction

---
+++ TickIt-Api-Gateway/src/main/java/com/harish/tickit/apigateway/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.harish.tickit.apigateway;
+
+import java.util.List;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil
+{
+	@Value("${jwt.secret}")
+	private String secretKey;
+	
+	public Boolean verifyJwt(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secretKey.getBytes());
+		Boolean res= Jwts.parser().verifyWith(key).build().parseSignedClaims(token) !=null;
+		return res;
+	}
+	
+	public List<String> getRoles(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secretKey.getBytes());
+		List<?> ls = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().get("roles", List.class);
+		
+		List<String> res= ls.stream()
+				            .map(i->{
+				            	return i.toString();
+				            })
+				            .toList();
+		return res;
+	}
+	
+	public String getUserName(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secretKey.getBytes());
+		
+		String res= Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
+		return res;
+	}
+	
+}
