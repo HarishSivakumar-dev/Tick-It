39f5a60389a4676c29cae909e653f3fb30513702
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-14 09:48:48 +0530

    refactor(JwtAuthFilter)

--- TickIt-Api-Gateway/src/main/java/com/harish/tickit/apigateway/JwtAuthFilter.java
+++ TickIt-Api-Gateway/src/main/java/com/harish/tickit/apigateway/JwtAuthFilter.java
@@ -2,7 +2,6 @@ package com.harish.tickit.apigateway;
 
 import java.io.IOException;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
