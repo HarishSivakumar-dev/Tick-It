3c4dc998a3b5b6282253169f71c8da34fba8c0c0
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-15 23:42:38 +0530

    feat(enums): add NotificationType enum for categorizing notifications

---
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/enums/NotificationType.java
@@ -0,0 +1,27 @@
+package com.harish.tickit.notificationService.enums;
+
+public enum NotificationType
+{
+    ASSIGNMENT,
+
+    STATUS_CHANGED,
+
+    COMMENT_ADDED,
+
+    TICKET_CREATED,
+
+    TICKET_UPDATED,
+
+    PROJECT_CREATED,
+
+    PROJECT_UPDATED,
+
+    USER_MENTIONED,
+
+    DEADLINE_REMINDER,
+
+    SYSTEM,
+
+    ANNOUNCEMENT
+
+}
