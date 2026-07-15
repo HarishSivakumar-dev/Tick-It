6665f55cbb8fa5058f87f7249e78cd6af6f98d60
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-15 11:05:17 +0530

    feat(application): create main application class for Tick-It Notification Service

---
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/TickItNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.harish.tickit.notificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickItNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickItNotificationServiceApplication.class, args);
+	}
+
+}
