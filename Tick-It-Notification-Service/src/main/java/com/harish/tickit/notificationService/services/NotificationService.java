f84492fac37c33e00dec71a73bda7ed5e67cb9ef
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-16 16:21:52 +0530

    feat(services): implement methods for updating notification status and retrieving unread notifications

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/services/NotificationService.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/services/NotificationService.java
@@ -1,12 +1,15 @@
 package com.harish.tickit.notificationService.services;
 
 import java.time.LocalDate;
-
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.harish.tickit.notificationService.dtos.NotificationDto;
+import com.harish.tickit.notificationService.dtos.NotificationResponseDto;
+import com.harish.tickit.notificationService.dtos.NotificationUpdateDto;
 import com.harish.tickit.notificationService.models.Notification;
 import com.harish.tickit.notificationService.repos.NotificationRepo;
+import jakarta.transaction.Transactional;
 
 @Service
 public class NotificationService
@@ -28,5 +31,53 @@ public class NotificationService
 		
 		return "Notification sent/saved";
 	}
+	
+	public String updateNotificationStatus(NotificationUpdateDto dto)
+	{
+		Notification nt= nr.findById(dto.getNotificationId()).orElseThrow(()-> new RuntimeException("Notification not found"));
+		nt.setRead(true);
+		nr.save(nt);
+		
+		return "Notification updated !";
+	}
+	
+	@Transactional
+	public String updateNotificationStatusAll(int userId)
+	{
+		List<Notification> nt=nr.findByUserIdAndReadFalse(userId);
+		nt.forEach(r->r.setRead(true));
+		
+		return "Updated";
+								
+	}
+	
+	public Integer getUnreadNotificationCount(int userId)
+	{
+		int count= nr.countByUserIdAndReadFalse(userId);
+		return count;
+	}
+	
+	public List<NotificationResponseDto> getAllUnreadNotifications(int userId)
+	{
+		List<NotificationResponseDto> res = nr.findByUserIdAndReadFalse(userId)
+										     .stream()
+										     .map(r->{
+										    	 NotificationResponseDto dto= new NotificationResponseDto();
+										    	 dto.setMessage(r.getMessage());
+										    	 dto.setNotificationId(r.getId());
+										    	 dto.setTitle(r.getTitle());
+										    	 dto.setType(r.getType());
+										    	 dto.setUserId(r.getUserId());
+										    	 
+										    	 return dto;
+										     })
+										     .toList();
+		
+		return res;
+	}
+	
+	
+	
+	
 
 }
