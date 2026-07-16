57d02ce082a32438c6462b9dd007b99b65d5d98f
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-16 16:18:10 +0530

    feat(dtos): add NotificationResponseDto for encapsulating notification response data

---
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/dtos/NotificationResponseDto.java
@@ -0,0 +1,45 @@
+package com.harish.tickit.notificationService.dtos;
+
+import com.harish.tickit.notificationService.enums.NotificationType;
+
+public class NotificationResponseDto
+{
+	private int userId;
+	private String title;
+	private String message;
+	private NotificationType type;
+	private int notificationId;
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public NotificationType getType() {
+		return type;
+	}
+	public void setType(NotificationType type) {
+		this.type = type;
+	}
+	public int getNotificationId() {
+		return notificationId;
+	}
+	public void setNotificationId(int notificationId) {
+		this.notificationId = notificationId;
+	}
+	
+	
+}
