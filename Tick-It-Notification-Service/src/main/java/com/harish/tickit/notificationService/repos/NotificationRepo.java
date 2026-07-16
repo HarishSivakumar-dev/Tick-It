67f7b5f24ce7bb3d5446f4c908c416ed9311f79c
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-16 16:17:59 +0530

    feat(repos): add methods to count unread notifications and find unread notifications by user ID

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/repos/NotificationRepo.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/repos/NotificationRepo.java
@@ -1,9 +1,12 @@
 package com.harish.tickit.notificationService.repos;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.harish.tickit.notificationService.models.Notification;
 
 public interface NotificationRepo extends JpaRepository<Notification, Integer>
 {
+	int countByUserIdAndReadFalse(int userid);
+	List<Notification> findByUserIdAndReadFalse(int userId);
 
 }
