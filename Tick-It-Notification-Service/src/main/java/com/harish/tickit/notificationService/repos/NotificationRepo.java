4a7f0a7665f908635f61474fb47c6dad3c2cdcde
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-17 23:39:10 +0530

    feat(repo): update NotificationRepo to use employeeId instead of userId for notification queries

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/repos/NotificationRepo.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/repos/NotificationRepo.java
@@ -2,11 +2,14 @@ package com.harish.tickit.notificationService.repos;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.harish.tickit.notificationService.models.Notification;
 
+@Repository
 public interface NotificationRepo extends JpaRepository<Notification, Integer>
 {
-	int countByUserIdAndReadFalse(int userid);
-	List<Notification> findByUserIdAndReadFalse(int userId);
+	int countByEmployeeIdAndReadFalseAndDeletedFalse(int employeeid);
+	List<Notification> findByEmployeeIdAndReadFalseAndDeletedFalseOrderByCreatedAtDesc(int employeeid);
+	List<Notification> findByEmployeeIdAndDeletedFalseOrderByCreatedAtDesc(int employeeid);
 
 }
