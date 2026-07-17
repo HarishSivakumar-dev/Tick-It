ef9aae9483598a564f3d1c0ee30fc01d9c19778b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-17 23:39:18 +0530

    feat(auth): enhance JwtUtil to extract employeeId and manage token validation

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/util/JwtUtil.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/util/JwtUtil.java
@@ -1,6 +1,61 @@
 package com.harish.tickit.notificationService.util;
 
+import java.util.List;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
 public class JwtUtil
 {
+	@Value("${jwt.secret}")
+	private String secret;
 
+	public Integer extractUserId(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secret.getBytes());
+		Integer id=Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().get("employeeId", Integer.class);
+		
+		return id;
+	}
+	
+	public Boolean validateToken(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secret.getBytes());
+		
+		try
+		{
+			Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
+			return true;
+		}
+		catch(Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public String getUsername(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secret.getBytes());
+		
+		String username= Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getSubject();
+		return username;
+	}
+	
+	public List<String> getRolesFromToken(String token)
+	{
+		SecretKey key= Keys.hmacShaKeyFor(secret.getBytes());
+		List<?> ls= Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().get("roles", List.class);
+		
+		List<String> res= ls.stream()
+							.map(r->{
+								return r.toString();
+										
+							})
+							.toList();
+		
+		return res;			
+	}
 }
