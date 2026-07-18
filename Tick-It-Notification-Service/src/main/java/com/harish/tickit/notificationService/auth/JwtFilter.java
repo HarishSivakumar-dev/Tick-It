286c7f30dc8b2351f910437e67204250b26b7f5d
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-18 15:11:41 +0530

    feat(auth): annotate JwtFilter with @Component for Spring management

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/auth/JwtFilter.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/auth/JwtFilter.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import com.harish.tickit.notificationService.util.JwtUtil;
 import jakarta.servlet.FilterChain;
@@ -14,6 +15,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
+@Component
 public class JwtFilter extends OncePerRequestFilter
 {
 	@Autowired
