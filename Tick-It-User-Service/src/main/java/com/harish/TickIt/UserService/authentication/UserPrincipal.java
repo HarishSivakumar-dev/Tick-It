596308d38f4fcd8d66482d0358c9f5a2c2943caa
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-18 15:11:29 +0530

    feat(auth): create UserPrincipal class to encapsulate employeeId and userName

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/authentication/UserPrincipal.java
@@ -0,0 +1,20 @@
+package com.harish.TickIt.UserService.authentication;
+
+public class UserPrincipal
+{
+	private Integer employeeId;
+	private String userName;
+	
+	public Integer getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
