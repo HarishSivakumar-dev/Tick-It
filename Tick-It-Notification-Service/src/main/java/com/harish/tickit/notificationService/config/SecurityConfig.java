7632ea649583c820b8ef8f6ff43e0058da534cb5
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-18 15:10:21 +0530

    feat(auth): integrate JwtFilter into SecurityConfig for JWT authentication

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/config/SecurityConfig.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/config/SecurityConfig.java
@@ -1,14 +1,21 @@
 package com.harish.tickit.notificationService.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.harish.tickit.notificationService.auth.JwtFilter;
 
 @Configuration
 public class SecurityConfig
 {
+	@Autowired
+	private JwtFilter jwtfilter;
+	
 	@Bean
 	public SecurityFilterChain securityConfig(HttpSecurity sec)
 	{
@@ -17,6 +24,7 @@ public class SecurityConfig
 				  .formLogin(r->r.disable())
 				  .sessionManagement(r->r.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 				  .authorizeHttpRequests(r->r.requestMatchers(" ").permitAll().anyRequest().authenticated())
+				  .addFilterBefore(jwtfilter, UsernamePasswordAuthenticationFilter.class)
 				  .build();
 	}
 
