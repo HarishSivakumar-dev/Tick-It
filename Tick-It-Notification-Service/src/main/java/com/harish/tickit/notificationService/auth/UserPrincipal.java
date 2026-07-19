55aff12b79767411c0fffaaf5ff7440ffc390d0a
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-19 22:50:16 +0530

    fix(auth): change employeeId type from Integer to Long in UserPrincipal class

--- Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/auth/UserPrincipal.java
+++ Tick-It-Notification-Service/src/main/java/com/harish/tickit/notificationService/auth/UserPrincipal.java
@@ -2,13 +2,13 @@ package com.harish.tickit.notificationService.auth;
 
 public class UserPrincipal
 {
-	private Integer employeeId;
+	private Long employeeId;
 	private String userName;
 	
-	public Integer getEmployeeId() {
+	public Long getEmployeeId() {
 		return employeeId;
 	}
-	public void setEmployeeId(Integer employeeId) {
+	public void setEmployeeId(Long employeeId) {
 		this.employeeId = employeeId;
 	}
 	public String getUserName() {
