4ae2ed596ba9ffeef5affd105be8ca593be3de4a
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-22 23:09:52 +0530

    fix(repo): update method signature to use employeeId instead of employeeIdId

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/repos/ProjectMembersRepo.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/repos/ProjectMembersRepo.java
@@ -10,7 +10,7 @@ import com.harish.TickIt.ProjectService.models.ProjectMembers;
 public interface ProjectMembersRepo extends JpaRepository<ProjectMembers, Integer>
 {
 	List<ProjectMembers> findByProjectIdIsNull();
-	List<ProjectMembers> findByEmployeeIdIdAndProjectIdNotNull(Long id);
+	List<ProjectMembers> findByEmployeeIdAndProjectIdNotNull(Long id);
 	List<ProjectMembers> findByProjectId(Long id);
 	List<ProjectMembers> findByEmployeeIdIn(List<Long> ls);
 }
