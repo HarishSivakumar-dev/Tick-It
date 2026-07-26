7f2327cc8935c9f6b203b5198de1f5b741764089
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-26 21:55:01 +0530

    feat(ticket): add TicketCommentController for managing ticket comments

---
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/controllers/TicketCommentController.java
@@ -0,0 +1,29 @@
+package com.harish.TickIt.TicketService.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.harish.TickIt.TicketService.dtos.TicketCommentDto;
+import com.harish.TickIt.TicketService.services.TicketCommentService;
+
+@RestController
+@RequestMapping("/api/tickets")
+public class TicketCommentController
+{
+	@Autowired
+	private TicketCommentService tcs;
+	
+	@PostMapping("/comment/{ticketId}/create")
+	public ResponseEntity<String> createComment(@RequestBody TicketCommentDto dto, @PathVariable int ticketId)
+	{
+		String res= tcs.createComment(ticketId, dto);
+		return ResponseEntity.status(HttpStatus.OK).body(res);
+	}
+	
+	
+}
