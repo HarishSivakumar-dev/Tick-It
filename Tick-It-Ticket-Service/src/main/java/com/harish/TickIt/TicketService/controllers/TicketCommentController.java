149e5963ab1a0f96c2f965371e8d5d31ffbffd8a
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-27 23:54:53 +0530

    feat(ticket): add endpoint to retrieve all comments for a specific ticket

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/controllers/TicketCommentController.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/controllers/TicketCommentController.java
@@ -1,14 +1,18 @@
 package com.harish.TickIt.TicketService.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.harish.TickIt.TicketService.dtos.TicketCommentDto;
+import com.harish.TickIt.TicketService.dtos.TicketCommentResponseDto;
 import com.harish.TickIt.TicketService.services.TicketCommentService;
 
 @RestController
@@ -23,6 +27,13 @@ public class TicketCommentController
 	{
 		String res= tcs.createComment(ticketId, dto);
 		return ResponseEntity.status(HttpStatus.OK).body(res);
+	}
+	
+	@GetMapping("/comment/get/{ticketId}")
+	public ResponseEntity<List<TicketCommentResponseDto>> getAllCommentsForTicket(@PathVariable int ticketId)
+	{
+		List<TicketCommentResponseDto> ls= new ArrayList<>();
+		return ResponseEntity.status(HttpStatus.OK).body(ls);
 	}
 	
 	
