480854765e47914a0d5f063d2b432df4ac4d981f
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-27 23:55:37 +0530

    feat(ticket): add repository method to find comments by ticket ID

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketCommentsRepo.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/repos/TicketCommentsRepo.java
@@ -1,9 +1,12 @@
 package com.harish.TickIt.TicketService.repos;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.harish.TickIt.TicketService.model.TicketComments;
 
+@Repository
 public interface TicketCommentsRepo extends JpaRepository<TicketComments, Integer>
 {
-
+	List<TicketComments> findByTicketId(int ticketId);
 }
