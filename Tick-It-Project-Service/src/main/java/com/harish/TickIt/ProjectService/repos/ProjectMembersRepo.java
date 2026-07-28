c3b3c6d378c3453f24f9114a655ed50b3ccd902b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-28 23:29:02 +0530

    feat(project): add method to check if project member exists by employee ID and project ID

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/repos/ProjectMembersRepo.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/repos/ProjectMembersRepo.java
@@ -3,7 +3,6 @@ package com.harish.TickIt.ProjectService.repos;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.harish.TickIt.ProjectService.models.ProjectMembers;
 
 @Repository
@@ -13,4 +12,5 @@ public interface ProjectMembersRepo extends JpaRepository<ProjectMembers, Intege
 	List<ProjectMembers> findByEmployeeIdAndProjectIdNotNull(Long id);
 	List<ProjectMembers> findByProjectId(Long id);
 	List<ProjectMembers> findByEmployeeIdIn(List<Long> ls);
+	Boolean existsByEmployeeIdAndProjectId(Long employeeId, long projectId);
 }
