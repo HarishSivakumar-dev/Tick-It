94169005494d663cc4737fc5cd131d9c3205b5be
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-28 15:11:47 +0530

    feat(ticket): add update and delete endpoints for ticket comments

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/controllers/TicketCommentController.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/controllers/TicketCommentController.java
@@ -1,15 +1,17 @@
 package com.harish.TickIt.TicketService.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.harish.TickIt.TicketService.dtos.TicketCommentDto;
 import com.harish.TickIt.TicketService.dtos.TicketCommentResponseDto;
@@ -32,8 +34,22 @@ public class TicketCommentController
 	@GetMapping("/comment/get/{ticketId}")
 	public ResponseEntity<List<TicketCommentResponseDto>> getAllCommentsForTicket(@PathVariable int ticketId)
 	{
-		List<TicketCommentResponseDto> ls= new ArrayList<>();
+		List<TicketCommentResponseDto> ls= tcs.getCommentsForTicket(ticketId);
 		return ResponseEntity.status(HttpStatus.OK).body(ls);
+	}
+	
+	@PutMapping("/comment/edit/{commentId}")
+	public ResponseEntity<String> updateAndAlterComment(@PathVariable int commentId, @RequestParam String message)
+	{
+		String res= tcs.updateComment(commentId, message);
+		return ResponseEntity.status(HttpStatus.OK).body(res);
+	}
+	
+	@DeleteMapping("/comment/delete/{commentId}")
+	public ResponseEntity<String> deleteComments(@PathVariable int commentId)
+	{
+		String res= tcs.softDeleteEmployeeComment(commentId);
+		return ResponseEntity.status(HttpStatus.OK).body(res);
 	}
 	
 	
