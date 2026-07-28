d96647b43432369fff9a16f8f7d36d240c0a49a7
Harish Sivakumar <harishss.2k07@gmail.com>
2026-07-28 15:12:23 +0530

    feat(ticket): implement soft delete and update functionality for ticket comments

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/services/TicketCommentService.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/services/TicketCommentService.java
@@ -2,6 +2,7 @@ package com.harish.TickIt.TicketService.services;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import com.harish.TickIt.TicketService.dtos.TicketCommentDto;
 import com.harish.TickIt.TicketService.dtos.TicketCommentResponseDto;
 import com.harish.TickIt.TicketService.model.TicketComments;
 import com.harish.TickIt.TicketService.repos.TicketCommentsRepo;
+import jakarta.transaction.Transactional;
 
 @Service
 public class TicketCommentService
@@ -28,6 +30,7 @@ public class TicketCommentService
 		tc.setTicketId(ticketId);
 		tc.setEmployeeName(user.getUserName());
 		tc.setMessage(dto.getMessage());
+		tc.setDeleted(Boolean.FALSE);
 
 		if(dto.getParentCommentId()==null)
 		{
@@ -35,7 +38,15 @@ public class TicketCommentService
 		}
 		else
 		{
-			tc.setParentCommentId(dto.getParentCommentId());
+			Optional<TicketComments> res= tcr.findById(dto.getParentCommentId());
+			if(res.isPresent())
+			{
+				tc.setParentCommentId(dto.getParentCommentId());
+			}
+			else
+			{
+				throw new RuntimeException("NO TICKET FOUND");
+			}
 		}
 		
 		tcr.save(tc);
@@ -45,7 +56,7 @@ public class TicketCommentService
 	
 	public List<TicketCommentResponseDto> getCommentsForTicket(int ticketId)
 	{
-		List<TicketCommentResponseDto> res= tcr.findByTicketId(ticketId)
+		List<TicketCommentResponseDto> res= tcr.findByTicketIdAndDeletedFalseOrderByCreatedAtAsc(ticketId)
 											   .stream()
 											   .map(r->{
 												   TicketCommentResponseDto dt= new TicketCommentResponseDto();
@@ -61,5 +72,43 @@ public class TicketCommentService
 											   .toList();
 		return res;
 	}
+
+	@Transactional
+	public String updateComment(int commentId, String message) 
+	{
+		TicketComments tc= tcr.findById(commentId).orElseThrow(()-> new RuntimeException("NO COMMENT FOUND"));
+		UserPrincipal up= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		if((tc.getEmployeeId().equals(up.getEmployeeId())) && tc.getDeleted().equals(Boolean.FALSE))
+		{
+			tc.setMessage(message);
+			tc.setUpdatedAt(LocalDateTime.now());
+		}
+		else
+		{
+			throw new RuntimeException("This action is not allowed");
+		}
+		
+		return "Comment updated";
+	}
+	
+	public String softDeleteEmployeeComment(int commentId)
+	{
+		TicketComments tc= tcr.findById(commentId).orElseThrow(()-> new RuntimeException("NO COMMENT FOUND"));
+		UserPrincipal up= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		if((tc.getEmployeeId().equals(up.getEmployeeId())) && tc.getDeleted().equals(Boolean.FALSE))
+		{
+			tc.setDeleted(Boolean.TRUE);
+			tc.setUpdatedAt(LocalDateTime.now());
+		}
+		else
+		{
+			throw new RuntimeException("This action is not allowed");
+		}
+		
+		return "Comment updated";
+	}
+	
 
 }
