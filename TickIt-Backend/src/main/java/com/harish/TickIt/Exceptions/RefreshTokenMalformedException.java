db4c35a0981b5e6848b9b9b431b6c08ce690d1d0
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-02 23:02:17 +0530

    style(auth): add serialVersionUID to RefreshTokenMalformedException for serialization compatibility

--- TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenMalformedException.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenMalformedException.java
@@ -1,5 +1,11 @@
 package com.harish.TickIt.Exceptions;
 
-public class RefreshTokenMalformedException extends Exception {
-
+public class RefreshTokenMalformedException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	
+	public RefreshTokenMalformedException(String message)
+	{
+		super(message);
+	}
 }
