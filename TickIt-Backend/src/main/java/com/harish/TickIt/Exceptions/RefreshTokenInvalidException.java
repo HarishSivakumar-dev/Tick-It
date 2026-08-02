96b71195f6549c262e9c23a62096a39cfd4d46d1
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-02 23:02:07 +0530

    style(auth): add serialVersionUID to RefreshTokenInvalidException for serialization compatibility

--- TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenInvalidException.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenInvalidException.java
@@ -1,5 +1,11 @@
 package com.harish.TickIt.Exceptions;
 
-public class RefreshTokenInvalidException extends Exception {
+public class RefreshTokenInvalidException extends Exception
+{
+	private static final long serialVersionUID = 1L;
+	public RefreshTokenInvalidException(String message)
+	{
+		super(message);
+	}
 
 }
