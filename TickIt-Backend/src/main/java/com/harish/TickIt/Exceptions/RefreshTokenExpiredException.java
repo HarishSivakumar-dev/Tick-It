e1db906bbd055b21fd5b9bd69b5b3063933a25f5
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-02 23:01:58 +0530

    style(auth): add serialVersionUID to RefreshTokenExpiredException for serialization compatibility

--- TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenExpiredException.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenExpiredException.java
@@ -2,5 +2,9 @@ package com.harish.TickIt.Exceptions;
 
 public class RefreshTokenExpiredException extends Exception 
 {
-
+	private static final long serialVersionUID = 1L;
+	public RefreshTokenExpiredException(String message) 
+	{
+		super(message);
+	}
 }
