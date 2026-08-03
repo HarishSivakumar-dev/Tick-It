ff5fea159b2488313f334106b02873919db1d17b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-03 22:35:40 +0530

    feat(auth): change RefreshTokenInvalidException to extend RuntimeException for improved error handling

--- TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenInvalidException.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/Exceptions/RefreshTokenInvalidException.java
@@ -1,6 +1,6 @@
 package com.harish.TickIt.Exceptions;
 
-public class RefreshTokenInvalidException extends Exception
+public class RefreshTokenInvalidException extends RuntimeException
 {
 	private static final long serialVersionUID = 1L;
 	public RefreshTokenInvalidException(String message)
