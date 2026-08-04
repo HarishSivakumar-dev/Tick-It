75ffc63504bdedc76693f7d59ab1f46349cf5a25
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-04 13:24:20 +0530

    feat(auth): add scheduled task for clearing expired sessions

--- TickIt-Backend/src/main/java/com/harish/TickIt/schedulers/ExpiredSessionClearanceScheduler.java
+++ TickIt-Backend/src/main/java/com/harish/TickIt/schedulers/ExpiredSessionClearanceScheduler.java
@@ -1,9 +1,25 @@
 package com.harish.TickIt.schedulers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import com.harish.TickIt.repositories.SessionManagementRepo;
 
 @Component
 public class ExpiredSessionClearanceScheduler
 {
+	@Autowired
+	private SessionManagementRepo sessionManagementRepo;
+	
+	@Scheduled(cron = "0 0 * * * ?") // Runs every hour
+	public void clearExpiredSessions()
+	{
+		int rows= sessionManagementRepo.deleteByExpiresAtBeforeOrRevokedTrue(java.time.LocalDateTime.now());
+		
+		if(rows>0)
+		{
+			System.out.println("Expired sessions cleared: " + rows);
+		}
+	}
 
 }
