2b62d1582e0400bbd9214acea669a6d1533bacd8
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-08 22:51:04 +0530

    fix(rate-limiting): update Redis script to handle count increment and expiration logic

--- TickIt-Api-Gateway/src/main/resources/scripts/redisScript.lua
+++ TickIt-Api-Gateway/src/main/resources/scripts/redisScript.lua
@@ -4,15 +4,22 @@ local time= tonumber(ARGV[1])
 local window= tonumber(ARGV[2])
 local member= ARGV[3]
 
-redis.call('ZREMRANGEBYSCORE', key, 0, time - window)
 local count= redis.call('ZCARD', key)
 
-if count>9 then
-    return 0
-else
+if(count==0) then
     redis.call('ZADD', key,time,member)
-    redis.call('EXPIRE', key, 600)
+    redis.call('EXPIRE', key, window)
     return 1
+else
+	redis.call('ZREMRANGEBYSCORE', key, 0, time - window)
+	local count= redis.call('ZCARD', key)+1
+
+	if count>9 then
+    	return 0
+	else
+    	redis.call('ZADD', key,time,member)
+    	redis.call('EXPIRE', key, window)
+    	return 1
 end
 
 
