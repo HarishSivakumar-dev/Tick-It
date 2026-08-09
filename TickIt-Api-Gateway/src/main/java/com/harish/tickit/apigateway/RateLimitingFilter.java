58eebcef6cd732464e8adeaf7818b06b9dbaf05b
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-09 21:32:24 +0530

    fix(rate-limiting): enhance rate limiting logic to support dynamic limits based on request paths

--- TickIt-Api-Gateway/src/main/java/com/harish/tickit/apigateway/RateLimitingFilter.java
+++ TickIt-Api-Gateway/src/main/java/com/harish/tickit/apigateway/RateLimitingFilter.java
@@ -3,21 +3,31 @@ package com.harish.tickit.apigateway;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Collections;
-
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+@Component
 public class RateLimitingFilter extends org.springframework.web.filter.OncePerRequestFilter
 {
 	@Autowired
 	private RedisTemplate<String, Object> redisTemplate;
 	@Autowired
 	private org.springframework.data.redis.core.script.DefaultRedisScript<Long> redisScript;
+	
+	private static final Map<String, Integer> rq= Map.of(
+			"/api/register", 10,
+			"/api/login", 10,
+			"/oauth2/authorization/google", 10,
+			"/login/oauth2/code/google", 20
+	);
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -25,19 +35,37 @@ public class RateLimitingFilter extends org.springframework.web.filter.OncePerRe
 	{
 		String req= request.getRequestURI();
 		String ip= request.getHeader("X-Forwarded-For");
-		String id= request.getHeader("X-EmployeeId");
+		String key;
+		
+		if(!rq.containsKey(req))
+		{
+			filterChain.doFilter(request, response);
+			return;
+		}
 		
 		if(ip==null || ip.isEmpty() || ip.equalsIgnoreCase("unknown"))
 		{
 			ip= request.getRemoteAddr();
 		}
 		
-		String key= "rate_limit:"+id+":"+ip;
+		if(SecurityContextHolder.getContext().getAuthentication()!=null && SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof UserPrincipal)
+		{
+			UserPrincipal up= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			String id= String.valueOf(up.getEmployeeId());
+			key = "rate_limit:" + req + ":" + id + ":" + ip;
+		}
+		else
+		{
+			key = "rate_limit:" + req + ":" + ip;
+		}
+		
+		
 		long sec= Instant.now().getEpochSecond();
 		long time= System.currentTimeMillis();
 		long window= 600;
+		int limit= rq.get(req);
 		
-		long count=redisTemplate.execute(redisScript,Collections.singletonList(key),sec,window,time);
+		long count=redisTemplate.execute(redisScript,Collections.singletonList(key),sec,window,time, limit);
 		
 		if(count==0)
 		{
@@ -46,7 +74,8 @@ public class RateLimitingFilter extends org.springframework.web.filter.OncePerRe
 			return;
 		}
 		
-		filterChain.doFilter(request, response);	
+		filterChain.doFilter(request, response);
+		return;
 	}
 
 }
