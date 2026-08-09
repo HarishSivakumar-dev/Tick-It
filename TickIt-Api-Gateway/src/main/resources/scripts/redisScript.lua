28c387b6c3523791c9b0a75e85b905170c84e106
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-09 21:32:35 +0530

    fix(rate-limiting): update Redis script to use dynamic limit for request count

--- TickIt-Api-Gateway/src/main/resources/scripts/redisScript.lua
+++ TickIt-Api-Gateway/src/main/resources/scripts/redisScript.lua
@@ -3,6 +3,7 @@ local key= KEYS[1]
 local time= tonumber(ARGV[1])
 local window= tonumber(ARGV[2])
 local member= ARGV[3]
+local limit= tonumber(ARGV[4])
 
 local count= redis.call('ZCARD', key)
 
@@ -14,7 +15,7 @@ else
 	redis.call('ZREMRANGEBYSCORE', key, 0, time - window)
 	local count= redis.call('ZCARD', key)+1
 
-	if count>9 then
+	if count>limit then
     	return 0
 	else
     	redis.call('ZADD', key,time,member)
