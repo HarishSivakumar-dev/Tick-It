0ced1fe711a28098de4a2a988bf1349d0df5d6c7
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-12 23:47:53 +0530

    feat(redis): add ObjectMapper support for RedisTemplate configuration

--- Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/RedisConfig.java
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/RedisConfig.java
@@ -1,21 +1,38 @@
 package com.harish.TickIt.UserService.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.harish.TickIt.UserService.dtos.ProfileResponseDto;
+import tools.jackson.databind.ObjectMapper;
+
 
 @Configuration 
 public class RedisConfig 
 {
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) 
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory, ObjectMapper om) 
 	{
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new JacksonJsonRedisSerializer<>(Object.class));
+		template.setValueSerializer(new JacksonJsonRedisSerializer<>(om,Object.class));
 		return template;
 	}
-
+	
+	@Bean
+	public RedisTemplate<String, ProfileResponseDto> redisTemplateUser(RedisConnectionFactory connectionFactory, ObjectMapper om)
+	{
+		RedisTemplate<String, ProfileResponseDto> template= new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new JacksonJsonRedisSerializer<>(om,ProfileResponseDto.class));
+		return template;
+	}
+	
+	
+	
 }
