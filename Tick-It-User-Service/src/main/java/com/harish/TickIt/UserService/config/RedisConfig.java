4b9c5148cf40f964b7856319d2be058eda1514fd
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-12 09:34:38 +0530

    feat(redis): add Redis configuration for custom RedisTemplate setup

---
+++ Tick-It-User-Service/src/main/java/com/harish/TickIt/UserService/config/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.harish.TickIt.UserService.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration 
+public class RedisConfig 
+{
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) 
+	{
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new JacksonJsonRedisSerializer<>(Object.class));
+		return template;
+	}
+
+}
