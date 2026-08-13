15b9e6446259d66a998229e68c5f70568434125c
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-13 12:36:41 +0530

    feat(redis): add RedisTemplate configuration for TicketResponseDto list

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/RedisConfig.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/config/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.harish.TickIt.TicketService.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import com.harish.TickIt.TicketService.dtos.TicketResponseDto;
 import tools.jackson.databind.ObjectMapper;
+import tools.jackson.databind.JavaType;
 
 @Configuration
 public class RedisConfig
@@ -18,6 +22,19 @@ public class RedisConfig
 		template.setConnectionFactory(connectionFactory);
 		template.setKeySerializer(new StringRedisSerializer());
 		template.setValueSerializer(new JacksonJsonRedisSerializer<>(mapper,Object.class));
+		return template;
+	}
+	
+	@Bean
+	public RedisTemplate<String, List<TicketResponseDto>> redisTemplateTickets(RedisConnectionFactory connectionFactory, ObjectMapper mapper)
+	{
+		JavaType tp= mapper.getTypeFactory().constructCollectionType(List.class, TicketResponseDto.class);
+		RedisTemplate<String, List<TicketResponseDto>> template = new RedisTemplate<>();
+		
+		template.setConnectionFactory(connectionFactory);
+		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new JacksonJsonRedisSerializer<>(mapper, tp));
+		
 		return template;
 	}
 	
