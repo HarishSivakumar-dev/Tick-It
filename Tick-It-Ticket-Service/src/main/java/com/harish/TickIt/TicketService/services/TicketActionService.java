70a2867830ad2214a1af5e6c0b206a404c9d9afa
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-14 20:06:43 +0530

    feat(service): integrate ProjectFeignClient and enhance ticket retrieval with Redis caching

--- Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/services/TicketActionService.java
+++ Tick-It-Ticket-Service/src/main/java/com/harish/TickIt/TicketService/services/TicketActionService.java
@@ -18,6 +18,7 @@ import com.harish.TickIt.TicketService.dtos.UserFeignDto;
 import com.harish.TickIt.TicketService.enums.TicketApprovalStatus;
 import com.harish.TickIt.TicketService.enums.TicketPriority;
 import com.harish.TickIt.TicketService.enums.TicketStatus;
+import com.harish.TickIt.TicketService.feign.ProjectFeignClient;
 import com.harish.TickIt.TicketService.feign.UserFeignClient;
 import com.harish.TickIt.TicketService.model.Ticket;
 import com.harish.TickIt.TicketService.model.TicketApprovalAudit;
@@ -36,6 +37,8 @@ public class TicketActionService
 	@Autowired
 	private TicketWrapperImpl ticketWrapperImpl;
 	@Autowired
+	private ProjectFeignClient pfc;
+	@Autowired
 	private UserFeignClient ufc;
 	@Autowired
 	private TicketApprovalAuditRepo trep;
@@ -47,7 +50,10 @@ public class TicketActionService
 		// This method will create a ticket
 		Ticket ticket = ticketWrapperImpl.createTicket(dto);
 		ticketRepo.save(ticket);
+		
 		redisTemplate.delete("projectTickets:"+dto.getProjectId());
+		redisTemplate.delete("AvailableTickets:"+dto.getProjectId());
+		
 		return "Ticket created successfully";
 	}
 	
@@ -55,6 +61,7 @@ public class TicketActionService
 	{
 		// This method will return all the tickets of a project
 		List<TicketResponseDto> tk= redisTemplate.opsForValue().get("projectTickets:"+projectId);
+		
 		if(tk==null)
 		{
 			List<Ticket> tickets = ticketRepo.findByProjectIdAndStatusNotAndAssignedToIsNull(projectId, TicketStatus.RESOLVED);
@@ -80,7 +87,10 @@ public class TicketActionService
 		if(ticketRepo.existsById(ticketId))
 		{
 			ticketRepo.deleteById(ticketId);
+			
 			redisTemplate.delete("projectTickets:"+projectId);
+			redisTemplate.delete("AvailableTickets:"+ projectId);
+			
 			return "Ticket deleted successfully";
 		}
 		else
@@ -108,10 +118,14 @@ public class TicketActionService
 			{
 				ticket.setAssignedTo(null);
 				ticket.setClosedAt(null);
+				redisTemplate.delete("userTickets:"+ticket.getAssignedEmployeeId());
+				ticket.setAssignedEmployeeId(null);
 			}
 			
 			ticketRepo.save(ticket);
+			
 			redisTemplate.delete("projectTickets:"+ticket.getProjectId());
+			redisTemplate.delete("AvailableTickets:"+ticket.getProjectId());
 			
 			return "Ticket status updated successfully";
 		}
@@ -138,46 +152,51 @@ public class TicketActionService
 		tk.setStatus(TicketStatus.IN_PROGRESS);
 		
 		ticketRepo.save(tk);
+		
 		redisTemplate.delete("projectTickets:"+tk.getProjectId());
+		redisTemplate.delete("AvailableTickets:"+tk.getProjectId());
+		redisTemplate.delete("userTickets:"+tk.getAssignedEmployeeId());
 		
 		return "User Assigned !";
 	}
 	
 	public List<TicketResponseDto> getAvailableTicketsForUser(int projectId)
 	{
-		List<TicketPriority> ls= new ArrayList<>();
-		ls.add(TicketPriority.CRITICAL);
-		ls.add(TicketPriority.HIGH);
 		
-		List<TicketResponseDto> dt= ticketRepo.findByProjectIdAndStatusAndPriorityNotInAndAssignedToIsNull(projectId,TicketStatus.OPEN,ls)
-											  .stream()
-											  .map(r->{
-												  
-												  TicketResponseDto dto= new TicketResponseDto();
-												  dto.setClosedAt(r.getClosedAt());
-												  dto.setCreatedAt(r.getCreatedAt());
-												  dto.setCreatedBy(r.getCreatedBy());
-												  dto.setCreatorId(r.getCreatorId());
-												  dto.setCreatorMail(r.getCreatorMail());
-												  dto.setCreatorProfilePictureUrl(r.getCreatorProfilePictureUrl());
-												  dto.setDescription(r.getDescription());
-												  dto.setId(r.getId());
-												  dto.setPriority(r.getPriority().toString());
-												  dto.setStatus(r.getStatus().toString());
-												  dto.setTitle(r.getTitle());
-												  dto.setUpdatedAt(r.getUpdatedAt());
-												  
-												  return dto;
-												  
-											  })
-											  .toList();
+		List<TicketResponseDto> res= redisTemplate.opsForValue().get("AvailableTickets:"+projectId);
 		
-		return dt;
+		if(res==null)
+		{
+			List<TicketPriority> ls= new ArrayList<>();
+			ls.add(TicketPriority.CRITICAL);
+			ls.add(TicketPriority.HIGH);
+		
+			List<TicketStatus> stat= new ArrayList<>();
+			stat.add(TicketStatus.OPEN);
+			stat.add(TicketStatus.REOPENED);
+			
+			List<TicketResponseDto> dt= ticketRepo.findByProjectIdAndStatusInAndPriorityNotInAndAssignedToIsNull(projectId,stat,ls)
+												  .stream()
+												  .map(r->{
+													  return ticketWrapperImpl.toDto(r);
+												  })
+												  .toList();
+			
+			redisTemplate.opsForValue().set("AvailableTickets:"+projectId, dt,10, TimeUnit.MINUTES);
+			return dt;
+		}
+		else
+		{
+			return res;
+		}
+		
 	}
 	
 	@Transactional
 	public String updateTicketApprovalStatus(TicketApprovalDto dto)
 	{
+		UserPrincipal up= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
 		if(dto.getStatus().equals(TicketApprovalStatus.PENDING))
 		{
 			return "Same State !"; 
@@ -190,6 +209,13 @@ public class TicketActionService
 			tk.setApproved(null);
 			tk.setClosedAt(null);
 			tk.setStatus(TicketStatus.REOPENED);
+			
+			redisTemplate.delete("userTickets:"+tk.getAssignedEmployeeId());
+			
+			tk.setAssignedEmployeeId(null);
+			tk.setAssignedTo(null);
+			
+			redisTemplate.delete("AvailableTickets:"+tk.getProjectId());
 		}
 		else
 		{
@@ -200,13 +226,14 @@ public class TicketActionService
 		
 		TicketApprovalAudit taa= new TicketApprovalAudit();
 		taa.setDescription(dto.getDescription());
-		taa.setHandledUserName(tk.getAssignedTo());
+		taa.setHandledUserName(up.getUserName());
 		taa.setStatus(dto.getStatus());
 		taa.setTicketId(tk);
 		
 		trep.save(taa);
 		
 		redisTemplate.delete("projectTickets:"+tk.getProjectId());
+		
 		return "Updated Successfully";
 	}
 	
@@ -215,23 +242,7 @@ public class TicketActionService
 		List<TicketResponseDto> res= ticketRepo.filterBySpecs(stat, prior, status)
 											   .stream()
 											   .map(r->{
-												   
-												   	  TicketResponseDto dto= new TicketResponseDto();
-												   	  dto.setClosedAt(r.getClosedAt());
-													  dto.setCreatedAt(r.getCreatedAt());
-													  dto.setCreatedBy(r.getCreatedBy());
-													  dto.setCreatorId(r.getCreatorId());
-													  dto.setCreatorMail(r.getCreatorMail());
-													  dto.setCreatorProfilePictureUrl(r.getCreatorProfilePictureUrl());
-													  dto.setDescription(r.getDescription());
-													  dto.setId(r.getId());
-													  dto.setPriority(r.getPriority().toString());
-													  dto.setStatus(r.getStatus().toString());
-													  dto.setTitle(r.getTitle());
-													  dto.setUpdatedAt(r.getUpdatedAt());
-													  
-													  return dto;
-												   
+												   return ticketWrapperImpl.toDto(r);
 											   })
 											   .toList();
 		return res;
@@ -240,29 +251,63 @@ public class TicketActionService
 	public List<TicketResponseDto> getUserTickets()
 	{
 		UserPrincipal prin= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		List<TicketResponseDto> res= ticketRepo.findByAssignedEmployeeIdOrderByCreatedAtDesc((long)prin.getEmployeeId())
-											   .stream()
-											   .map(r->{
-
-												   	  TicketResponseDto dto= new TicketResponseDto();
-												   	  dto.setClosedAt(r.getClosedAt());
-													  dto.setCreatedAt(r.getCreatedAt());
-													  dto.setCreatedBy(r.getCreatedBy());
-													  dto.setCreatorId(r.getCreatorId());
-													  dto.setCreatorMail(r.getCreatorMail());
-													  dto.setCreatorProfilePictureUrl(r.getCreatorProfilePictureUrl());
-													  dto.setDescription(r.getDescription());
-													  dto.setId(r.getId());
-													  dto.setPriority(r.getPriority().toString());
-													  dto.setStatus(r.getStatus().toString());
-													  dto.setTitle(r.getTitle());
-													  dto.setUpdatedAt(r.getUpdatedAt());
-													  
-													  return dto;
-											   })
-											   .toList();
 		
-		return res;
+		List<TicketResponseDto> dt=redisTemplate.opsForValue().get("userTickets:"+prin.getEmployeeId());
+		
+		if(dt==null)
+		{
+			List<TicketResponseDto> res= ticketRepo.findByAssignedEmployeeIdOrderByCreatedAtDesc((long)prin.getEmployeeId())
+					   .stream()
+					   .map(r->{
+						   return ticketWrapperImpl.toDto(r);
+					   })
+					   .toList();
+			
+			redisTemplate.opsForValue().set("userTickets:"+prin.getEmployeeId(), res, 10, TimeUnit.MINUTES);
+			
+			return res;
+		}
+		else
+		{
+			return dt;
+		}
+	}
+	
+	public List<TicketResponseDto> getAllTickets(int projectId)
+	{	
+		if(pfc.verifyEmployee(projectId).getBody())
+		{
+			List<TicketResponseDto> res= ticketRepo.findByProjectId(projectId)
+					   .stream()
+					   .map(r->{
+						   return ticketWrapperImpl.toDto(r);
+					   })
+					   .toList();
+			return res;
+		}
+		else
+		{
+			throw new RuntimeException("UNAUTHORIZED");
+		}
+		
+	}
+	
+	public List<TicketResponseDto> getUnapprovedTickets(int projectid, TicketApprovalStatus status)
+	{	
+		if(pfc.verifyEmployee(projectid).getBody())
+		{
+			List<TicketResponseDto> dto= ticketRepo.findByProjectIdAndApproved(projectid,status)
+					  .stream()
+					  .map(r->{
+						  return ticketWrapperImpl.toDto(r);
+					  })
+					  .toList();
+			return dto;
+		}
+		else
+		{
+			throw new RuntimeException("UNAUTHORIZED");
+		}
 	}
 	
 }
