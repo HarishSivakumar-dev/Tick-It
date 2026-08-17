ac0a685b940009764bd13dfa4c0fe2049fc6df82
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-17 15:08:43 +0530

    fix(controller): update authorization role and fetch user-specific projects in ProjectMemberController

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/controllers/ProjectMemberController.java
@@ -5,12 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.harish.TickIt.ProjectService.auth.UserPrincipal;
 import com.harish.TickIt.ProjectService.dtos.UserDetailsDto;
 import com.harish.TickIt.ProjectService.dtos.UserProjectDetailsDto;
 import com.harish.TickIt.ProjectService.dtos.UserProjectDto;
@@ -37,10 +40,11 @@ public class ProjectMemberController
 	}
 	
 	@GetMapping("/projects/myProjects/get")
-	@PreAuthorize("hasAnyRole('MANAGER', 'ADMIN')")
+	@PreAuthorize("hasRole('USER')")
 	public ResponseEntity<List<UserProjectDetailsDto>> getProjectsForUser()
 	{
-		return ResponseEntity.status(HttpStatus.OK).body(ps.getAllUserProjects());
+		UserPrincipal up= (UserPrincipal) SecurityContextHolder.getContext().getAuthentication();
+		return ResponseEntity.status(HttpStatus.OK).body(ps.getAllUserProjects(up.getEmployeeId()));
 	}
 	
 	@GetMapping("/projects/get/{projectId}")
