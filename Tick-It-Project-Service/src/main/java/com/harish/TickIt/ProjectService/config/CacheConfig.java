75332563245427563f6760b1acaafd2a6335703a
Harish Sivakumar <harishss.2k07@gmail.com>
2026-08-17 15:08:29 +0530

    feat(config): update cache configurations and serializers for project and user details

--- Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/config/CacheConfig.java
+++ Tick-It-Project-Service/src/main/java/com/harish/TickIt/ProjectService/config/CacheConfig.java
@@ -2,16 +2,20 @@ package com.harish.TickIt.ProjectService.config;
 
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.serializer.GenericJacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-
+import com.harish.TickIt.ProjectService.dtos.ProjectDetDto;
+import com.harish.TickIt.ProjectService.dtos.ProjectResponseDto;
+import com.harish.TickIt.ProjectService.dtos.UserDetailsDto;
+import com.harish.TickIt.ProjectService.dtos.UserProjectDetailsDto;
+import tools.jackson.databind.JavaType;
 import tools.jackson.databind.ObjectMapper;
 
 @Configuration
@@ -20,19 +24,34 @@ public class CacheConfig
 	@Bean
 	public RedisCacheManager cacheManager(RedisConnectionFactory factory, ObjectMapper om)
 	{
-		GenericJacksonJsonRedisSerializer serializer= new GenericJacksonJsonRedisSerializer(om);
+		JavaType ser= om.getTypeFactory().constructCollectionLikeType(List.class, ProjectResponseDto.class);
+		JavaType emp= om.getTypeFactory().constructCollectionType(List.class, UserDetailsDto.class);
+		JavaType m= om.getTypeFactory().constructCollectionType(List.class, UserProjectDetailsDto.class);
 		
 		RedisCacheConfiguration json= RedisCacheConfiguration.defaultCacheConfig()
 															 .entryTtl(Duration.ofMinutes(10))
-															 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+															 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(om, ser)));
+		RedisCacheConfiguration empl= RedisCacheConfiguration.defaultCacheConfig()
+															 .entryTtl(Duration.ofMinutes(10))
+															 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(om,emp)));
+		RedisCacheConfiguration usr= RedisCacheConfiguration.defaultCacheConfig()
+															.entryTtl(Duration.ofMinutes(10))
+															.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(om,m)));
+		RedisCacheConfiguration prj= RedisCacheConfiguration.defaultCacheConfig()
+															.entryTtl(Duration.ofMinutes(10))
+															.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(ProjectDetDto.class)));
+	
 		
 		Map<String, RedisCacheConfiguration> mp= new HashMap<String, RedisCacheConfiguration>();
 		mp.put("AllProjects", json);
+		mp.put("ProjectMembersUnassigned", empl);
+		mp.put("UserProjects", usr);
+		mp.put("ProjectMembers", usr);
+		mp.put("ProjectId", prj);
 		
 		return RedisCacheManager.builder(factory)
 								.withInitialCacheConfigurations(mp)
-								.build();
-				                
+								.build();			                
 	}
 
 }
